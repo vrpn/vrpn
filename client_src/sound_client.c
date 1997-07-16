0993b6806fa1e5a84129ad2383d87ca750476842
hudson <hudson>
1997-07-16 16:00:30 +0000

    (Taylor, Hudson, Larsen)   Made sound client use new naming scheme.

---
+++ client_src/sound_client.c
@@ -0,0 +1,10 @@
+#include "vrpn_Sound.h"
+main()
+{
+	vrpn_Sound_Remote *remote;
+	remote = new vrpn_Sound_Remote("sound@daytona");
+	remote->play_sampled_sound("/usr/lib/games/xboing/sounds/ddloo.au", 60,
+	                    vrpn_SND_SINGLE, vrpn_SND_BOTH);
+	while(1)
+	remote->mainloop();
+}
