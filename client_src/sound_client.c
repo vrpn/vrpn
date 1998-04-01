c110ab26fd1e673a0e7f00f9d006baf6963c039e
taylorr <taylorr>
1998-04-01 14:36:47 +0000

    This version has a sound client working with teh server on ioph100.

--- client_src/sound_client.c
+++ client_src/sound_client.c
@@ -1,26 +1,49 @@
 #include <unistd.h>
+#include <stdio.h>
 #include "vrpn_Sound.h"
-main()
+
+char	*SERVER = "Sound0@ioph100";
+char	*SOUND1 = "/net/nano/nano3/sounds/sfx-4/4-3.au";
+char	*SOUND2 = "/net/nano/nano3/sounds/sfx-3/3-2.au";
+
+int main()
 {
 	vrpn_Sound_Remote *remote;
-	remote = new vrpn_Sound_Remote("sound0@ioph100");
+	remote = new vrpn_Sound_Remote(SERVER);
+	printf("Opened sound server %s\n",SERVER);
 
-	remote->preload_sampled_sound("/net/nano/nano3/sounds/sfx-4/4-3.au");
+	printf("Preloading sound %s\n",SOUND1);
+	remote->preload_sampled_sound(SOUND1);
 	remote->mainloop();
 	sleep(5);
-	remote->play_sampled_sound("/net/nano/nano3/sounds/sfx-4/4-3.au", 100,
+
+	printf("Playing sound %s\n",SOUND1);
+	remote->play_sampled_sound(SOUND1, 100,
 	                    vrpn_SND_SINGLE, vrpn_SND_BOTH, 1);
 	remote->mainloop();
-	remote->play_sampled_sound("/net/nano/nano3/sounds/sfx-3/3-2.au", 60,
+	sleep(3);
+
+	printf("Playing looped sound %s\n",SOUND2);
+	remote->play_sampled_sound(SOUND2, 60,
 	                    vrpn_SND_LOOPED, vrpn_SND_BOTH, 2);
 	remote->mainloop();
 	sleep(3);
-	remote->play_sampled_sound("/net/nano/nano3/sounds/sfx-4/4-3.au", 100,
+
+	printf("Playing looped sound %s\n",SOUND1);
+	remote->play_sampled_sound(SOUND1, 100,
 	                    vrpn_SND_LOOPED, vrpn_SND_BOTH, 3);
 	remote->mainloop();
 	sleep(20);
 
+	printf("...stopping looped sound on channel 2...\n");
         remote->play_stop(2);
-	while(1)
+	printf("...stopping looped sound on channel 3...\n");
+        remote->play_stop(3);
 	remote->mainloop();
+
+	sleep (3);
+	remote->mainloop();
+
+	printf("...Done\n");
+	return 0;
 }
