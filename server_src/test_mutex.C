0f27ceda39566d44f3fcd4cde57aad49c801e00e
hudson <hudson>
2000-06-01 16:52:19 +0000

    Changed test_mutex to work with Server/Remote mutexes.  Kept the old test file around as test_peerMutex.

--- server_src/test_mutex.C
+++ server_src/test_mutex.C
@@ -1,69 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>  // for exit(), atoi()
 
+#include <stdlib.h>
 #include <vrpn_Mutex.h>
 
 
-int rg (void *) {
-  printf("callback - Mutex granted.\n");
-}
-
-int rd (void *) {
-  printf("callback - Mutex denied.\n");
-}
-
-int rel (void *) {
-  printf("callback - Mutex released.\n");
-}
-
 int main (int argc, char ** argv) {
 
-  vrpn_Mutex * me;
-  char inputLine [100];
+  vrpn_Synchronized_Connection * c;
+  vrpn_Mutex_Server * me;
+  int portno = 4500;
 
-  me = new vrpn_Mutex (argv[1], atoi(argv[2]));
-
-  me->addRequestGrantedCallback(NULL, rg);
-  me->addRequestDeniedCallback(NULL, rd);
-  me->addReleaseCallback(NULL, rel);
-
-  printf("req - request the mutex.\n");
-  printf("rel - release the mutex.\n");
-  printf("? - get current mutex state.\n");
-  printf("add - add a peer site to the mutex.\n");
-  printf("quit - exit.\n");
-
-  while (1) {
-
-    me->mainloop();
-    memset(inputLine, 0, 100);
-    fgets(inputLine, 100, stdin);
-
-    if (!strncmp(inputLine, "req", 3)) {
-      printf("test_mutex:  sending request.\n");
-      me->request();
-    } else if (!strncmp(inputLine, "rel", 3)) {
-      printf("test_mutex:  sending release.\n");
-      me->release();
-    } else if (!strncmp(inputLine, "?", 1)) {
-      printf("isAvailable:  %d.\n", me->isAvailable());
-      printf("isHeldLocally:  %d.\n", me->isHeldLocally());
-      printf("isHeldRemotely:  %d.\n", me->isHeldRemotely());
-      printf("numPeers:  %d.\n", me->numPeers());
-    } else if (!strncmp(inputLine, "add", 3)) {
-      printf("Name of peer:  ");
-      fflush(stdout);
-      fgets(inputLine, 100, stdin);
-      me->addPeer(inputLine);
-    } else if (!strncmp(inputLine, "quit", 4)) {
-      delete me;
-      exit(0);
-    } else {
-      printf(".\n");
-    }
+  if (argc > 2) {
+    portno = atoi(argv[2]);
   }
 
+  c = new vrpn_Synchronized_Connection (portno);
+  me = new vrpn_Mutex_Server (argv[1], c);
+
+  while (1) {
+    me->mainloop();
+  }
 
 }
 
