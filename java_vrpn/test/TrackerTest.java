9c410efe49cc74532a84691f61037f55680924f9
marshbur <marshbur>
2002-06-07 21:39:29 +0000

    * .cvsignore:  new 	* TrackerTest.java:  changes to keep up with vrpn.TrackerRemote 	(velocity, acceleration and exception) 	* test.vjp:  build setting (CLASSPATH)

--- java_vrpn/test/TrackerTest.java
+++ java_vrpn/test/TrackerTest.java
@@ -9,39 +9,51 @@ public class TrackerTest
 	public void trackerPositionUpdate( TrackerRemote.TrackerUpdate u,
 									   TrackerRemote tracker )
 	{
-		System.out.println( "Tracker message from vrpn: \n" +
+		System.out.println( "Tracker position message from vrpn: \n" /*+
 							"\ttime:  " + u.msg_time.getTime( ) + "  sensor:  " + u.sensor + "\n" +
 							"\tposition:  " + u.pos[0] + " " + u.pos[1] + " " + u.pos[2] + "\n" +
 							"\torientation:  " + u.quat[0] + " " + u.quat[1] + " " +
-							u.quat[2] + " " + u.quat[3] );
+							u.quat[2] + " " + u.quat[3] */ );
 	}
 	
 	public void trackerVelocityUpdate( TrackerRemote.VelocityUpdate v,
 									   TrackerRemote tracker )
 	{
-		System.out.println( "Tracker message from vrpn: \n" +
+		System.out.println( "Tracker velocity message from vrpn: \n" /* +
 							"\ttime:  " + v.msg_time.getTime( ) + "  sensor:  " + v.sensor + "\n" +
-							"\tposition:  " + v.vel[0] + " " + v.vel[1] + " " + v.vel[2] + "\n" +
+							"\tvelocity:  " + v.vel[0] + " " + v.vel[1] + " " + v.vel[2] + "\n" +
 							"\torientation:  " + v.vel_quat[0] + " " + v.vel_quat[1] + " " +
 							v.vel_quat[2] + " " + v.vel_quat[3] + "\n" +
-							"\t quat dt:  " + v.vel_quat_dt );
+							"\t quat dt:  " + v.vel_quat_dt */ );
 	}
 	
 	public void trackerAccelerationUpdate( TrackerRemote.AccelerationUpdate a,
 										   TrackerRemote tracker )
 	{
-		System.out.println( "Acceleration message from vrpn: \n" +
+		System.out.println( "Tracker acceleration message from vrpn: \n" /* +
 							"\ttime:  " + a.msg_time.getTime( ) + "  sensor:  " + a.sensor + "\n" +
 							"\tposition:  " + a.acc[0] + " " + a.acc[1] + " " + a.acc[2] + "\n" +
 							"\torientation:  " + a.acc_quat[0] + " " + a.acc_quat[1] + " " +
 							a.acc_quat[2] + " " + a.acc_quat[3] + "\n" +
-							"\t quat dt:  " + a.acc_quat_dt );
+							"\t quat dt:  " + a.acc_quat_dt */ );
 	}
 	
 
 	public static void main( String[] args )
 	{
-		TrackerRemote tracker = new TrackerRemote( "Phantom@europium-cs" );
+		String trackerName = "Phantom@tungsten-cs";
+		TrackerRemote tracker = null;
+		try
+		{
+			tracker = new TrackerRemote( trackerName );
+		}
+		catch( InstantiationException e )
+		{
+			// do something b/c you couldn't create the tracker
+			System.out.println( "We couldn't connect to tracker " + trackerName + "." );
+			System.out.println( e.getMessage( ) );
+			return;
+		}
 		
 		TrackerTest test = new TrackerTest( );
 		tracker.addPositionChangeListener( test );
