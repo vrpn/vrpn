26596a2a48046bcc641ef85a9984453b8704eddb
marshbur <marshbur>
2002-06-12 19:09:56 +0000

    2002-06-12  Tatsuhiro Segi  <segi@email.unc.edu>
    	* java_vrpn.dsw:  added ButtonRemote.dsp project
    	* java_vrpn.vjp:  added ButtonRemote.java
    	* ButtonRemote.dsp:  new project file
    	* ButtonRemote.java:  new.  the java part of the vrpn button remote
    	* vrpn_ButtonRemote.cpp:  new. c++ part of the same thing
    	* .cvsignore:  added vrpn_ButtonRemote.h

---
+++ java_vrpn/ButtonRemote.java
@@ -0,0 +1,176 @@
+package vrpn;
+import java.util.*;
+
+public class ButtonRemote implements Runnable
+{
+	
+	//////////////////
+	// Public structures and interfaces
+	
+	public class ButtonUpdate
+	{
+		public Date msg_time = new Date( );
+		public int button = 0; //Which button.  Starts from 0
+		public int state = 0; //0 for off, 1 for on
+	}
+	
+	public interface ButtonChangeListener
+	{
+		public void buttonUpdate( ButtonUpdate u, ButtonRemote button );
+	}
+
+	// end of the public structures and interfaces
+	///////////////////
+	
+	///////////////////
+	// Public methods
+	
+	public ButtonRemote( String name ) throws InstantiationException
+	{
+		try { this.init( name ); }
+		catch( java.lang.UnsatisfiedLinkError e )
+		{  
+			System.out.println( "Error initializing remote button " + name + "." );
+			System.out.println( " -- Unable to find native library." );
+			throw new InstantiationException( e.getMessage( ) );
+		}
+		
+		this.buttonThread = new Thread( this );
+		this.buttonThread.start( );
+	}
+	
+	public synchronized void addButtonChangeListener( ButtonChangeListener listener )
+	{
+		changeListeners.addElement( listener );
+	}
+	
+	/**
+	 * @return true on success; false on failure
+	 */
+	public synchronized boolean removeButtonChangeListener( ButtonChangeListener listener )
+	{
+		return changeListeners.removeElement( listener );
+	}
+	
+	/**
+	 * This should <b>not</b> be called by user code.
+	 */
+	public void run( )
+	{
+		while( keepRunning )
+		{
+			this.mainloop( );
+			try { Thread.sleep( 100 ); }
+			catch( InterruptedException e ) { } 
+		}
+	}
+	
+	// end public methods
+	////////////////////////
+	
+	
+	////////////////////////
+	// Protected methods
+	//
+	
+	/**
+	 * Should be called only by mainloop(), a native method which is itself
+	 * synchronized.  By extension, this method is synchronized (in that, for
+	 * a given TrackerRemote object, this method can only be called from one
+	 * thread at a time).
+	 */
+	protected void handleButtonChange( long tv_sec, long tv_usec, int button,
+									   int state )
+	{
+		// putting the body of this function into a synchronized block prevents
+		// other instances of ButtonRemote from calling listeners' buttonPositionUpdate
+		// method concurrently.
+		synchronized( notifyingChangeListenersLock )
+		{
+			ButtonUpdate b = new ButtonUpdate();
+			b.msg_time.setTime( tv_sec * 1000 + tv_usec );
+			b.button = button;
+			b.state = state;
+			
+			// notify all listeners
+			Enumeration e = changeListeners.elements( );
+			while( e.hasMoreElements( ) )
+			{
+				ButtonChangeListener l = (ButtonChangeListener) e.nextElement( );
+				l.buttonUpdate( b, this );
+			}
+		} // end synchronized( notifyingChangeListenersLock )
+	} // end method handleTrackerChange
+	
+	
+	/**
+	 * Initialize the native button object
+	 * @param name The name of the button and host (e.g., <code>"Button0@myhost.edu"</code>).
+	 * @return <code>true</code> if the button was connected successfully, 
+	 *			<code>false</code> otherwise.
+	 */
+	protected native boolean init( String name );
+
+	/**
+	 * This should only be called from the method finalize()
+	 */
+	protected native void shutdownButton( );
+	
+	protected synchronized native void mainloop( );
+	
+	protected void finalize( ) throws Throwable
+	{
+		keepRunning = false;
+		while( buttonThread.isAlive( ) )
+		{
+			try { buttonThread.join( ); }
+			catch( InterruptedException e ) { }
+		}
+		changeListeners.removeAllElements( );
+		this.shutdownButton( );
+	}
+	
+	
+	// end protected methods
+	///////////////////////
+	
+	///////////////////
+	// data members
+	
+	// this is used by the native code to store a C++ pointer to the 
+	// native vrpn_ButtonRemote object
+	protected int native_button = -1;
+	
+	// this is used to stop and to keep running the button thread
+	// in an orderly fashion.
+	protected boolean keepRunning = true;
+	
+	// the button thread
+	Thread buttonThread = null;
+	
+	protected Vector changeListeners = new Vector( );
+	
+	/**
+	 * these notifying*ListenersLock variables are used to ensure that multiple
+	 * ButtonRemote objects running in multiple threads don't call the 
+	 * buttonChangeUpdate, et. al., method of some single object concurrently.
+	 * For example, the handleButtonChange(...) method, which is invoked from native 
+	 * code, gets a lock on the notifyingChangeListenersLock object.  Since that object
+	 * is static, all other instances of ButtonRemote must wait before notifying 
+	 * their listeners and completing their handleButtonChange(...) methods.
+	 * They are necessary, in part, because methods in an interface can't be declared
+	 * synchronized (and the semantics of the keyword 'synchronized' aren't what's
+	 * wanted here, anyway -- we want synchronization across all instances, not just a 
+	 * single object).
+	 */
+	protected final static Object notifyingChangeListenersLock = new Object( );
+
+	// static initialization
+	static 
+	{
+		System.loadLibrary( "ButtonRemote" );
+	}
+	
+}
+
+
---
+++ java_vrpn/vrpn_ButtonRemote.cpp
@@ -0,0 +1,204 @@
+#include <stdio.h>
+#include <jni.h>
+#include "java_vrpn.h"
+#include "vrpn_Button.h"
+#include "vrpn_ButtonRemote.h"
+
+JavaVM* jvm = NULL;
+jclass jclass_vrpn_ButtonRemote = NULL;
+jfieldID jfid_vrpn_ButtonRemote_native_tracker = NULL;
+
+// This is called when the Java Virtual Machine loads this library
+//  and sets some global references that are used elsewhere.
+JNIEXPORT jint JNICALL JNI_OnLoad( JavaVM* jvm, void* reserved )
+{
+  //////////////
+  // set the jvm reference
+  ::jvm = jvm;
+
+  ///////////////
+  // get the JNIEnv pointer
+  JNIEnv* env;
+  if( jvm->GetEnv( (void**) &env, JAVA_VRPN_JNI_VERSION ) != JNI_OK )
+  {
+    printf( "Error loading vrpn ButtonRemote native library.\n" );
+    return JNI_ERR;
+  }
+  
+  ///////////////
+  // get the jclass reference to vrpn.ButtonRemote
+  jclass cls = env->FindClass( "vrpn/ButtonRemote" );
+  if( cls == NULL )
+  {
+    printf( "Error loading vrpn ButtonRemote native library "
+            "while trying to find class vrpn.ButtonRemote.\n" );
+    return JNI_ERR;
+  }
+
+  // make a global reference so the class can be referenced later.
+  // this must be a _WEAK_ global reference, so the class will be
+  // garbage collected, and so the JNI_OnUnLoad handler will be called
+  jclass_vrpn_ButtonRemote = (jclass) env->NewWeakGlobalRef( cls );
+  if( jclass_vrpn_ButtonRemote == NULL )
+  {
+    printf( "Error loading vrpn ButtonRemote native library "
+            "while setting up class vrpn.ButtonRemote.\n" );
+    return JNI_ERR;
+  }
+
+  ////////////////
+  // get a jfid field id reference to the "native_button" 
+  // field of class vrpn.ButtonRemote.
+  // field ids do not have to be made into global references.
+  jfid_vrpn_ButtonRemote_native_tracker 
+    = env->GetFieldID( jclass_vrpn_ButtonRemote, "native_button", "I" );
+  if( jfid_vrpn_ButtonRemote_native_tracker == NULL )
+  {
+    printf( "Error loading vrpn ButtonRemote native library "
+            "while looking into class vrpn.ButtonRemote.\n" );
+    return JNI_ERR;
+  }
+  
+  return JAVA_VRPN_JNI_VERSION;
+} // end JNI_OnLoad
+
+
+JNIEXPORT void JNICALL JNI_OnUnload( JavaVM* jvm, void* reserved )
+{
+  // get the JNIEnv pointer
+  JNIEnv* env;
+  if( jvm->GetEnv( (void**) &env, JAVA_VRPN_JNI_VERSION ) != JNI_OK )
+  {
+    printf( "Error unloading vrpn ButtonRemote native library.\n" );
+    return;
+  }
+
+  // delete the global reference to the vrpn.ButtonRemote class
+  env->DeleteWeakGlobalRef( jclass_vrpn_ButtonRemote );
+}
+
+
+// This is the callback for vprn to notify us of a new button message
+// --- WHAT IS TRACKERCB???????????  I just replace is with BUTTONCB...
+void handle_button_change( void* userdata, const vrpn_BUTTONCB info )
+{
+  if( jvm == NULL )
+    return;
+
+  /*
+  printf( "tracker change (C):  time:  %d.%d;  sensor:  %d;\n"
+          "\tpos: %f %f %f;\n"
+          "\tquat:  %f %f %f %f\n", info.msg_time.tv_sec, info.msg_time.tv_usec,
+          info.sensor, info.pos[0], info.pos[1], info.pos[2],
+          info.quat[0], info.quat[1], info.quat[2], info.quat[3] );
+  */
+
+  JNIEnv* env;
+  jvm->AttachCurrentThread( (void**) &env, NULL );
+  
+  jobject jobj = (jobject) userdata;
+  jclass jcls = env->GetObjectClass( jobj );
+  jmethodID jmid = env->GetMethodID( jcls, "handleButtonChange", "(JJII)V" );
+  if( jmid == NULL )
+  {
+    printf( "Warning:  vrpn_ButtonRemote library was unable to find the "
+            "Java method \'handleButtonChange\'.  This may indicate a version mismatch.\n" );
+    return;
+  }
+  env->CallVoidMethod( jobj, jmid, (jlong) info.msg_time.tv_sec, (jlong) info.msg_time.tv_usec,
+                       (jint) info.button, (jint) info.state );
+
+}
+
+
+JNIEXPORT void JNICALL 
+Java_vrpn_ButtonRemote_mainloop( JNIEnv* env, jobject jobj )
+{
+  // look up the button pointer
+  jclass jcls = env->GetObjectClass( jobj );
+  jfieldID jfid = env->GetFieldID( jcls, "native_button", "I" );
+  if( jfid == NULL )
+  {
+    printf( "Error in native method \"mainloop\":  unable to ID native button field.\n" );
+    return;
+  }
+  vrpn_Button_Remote* t = (vrpn_Button_Remote*) env->GetIntField( jobj, jfid );
+  if( t <= 0 )  // this button is uninitialized or has been shut down already
+    return;
+
+  // now call mainloop
+  t->mainloop( );
+}
+
+
+JNIEXPORT jboolean JNICALL 
+Java_vrpn_ButtonRemote_init( JNIEnv* env, jobject jobj, jstring jname )
+{
+  printf( "in Java_ButtonRemote_init(...)\n" );
+
+  // look up where to store the button pointer
+  jclass jcls = env->GetObjectClass( jobj );
+  jfieldID jfid = env->GetFieldID( jcls, "native_button", "I" );
+  if( jfid == NULL )
+  {
+    printf( "Error in native method \"init\":  unable to ID native button field.\n" );
+    return false;
+  }
+
+  // make a global reference to the Java ButtonRemote
+  // object, so that it can be referenced in the function
+  // handle_button_change(...)
+  jobj = env->NewGlobalRef( jobj );
+
+  // create the button
+  const char* name = env->GetStringUTFChars( jname, NULL );
+  vrpn_Button_Remote* t = new vrpn_Button_Remote( name );
+  t->register_change_handler( jobj, handle_button_change );
+  env->ReleaseStringUTFChars( jname, name );
+  
+  // now stash 't' in the jobj's 'native_button' field
+  jint jt = (jint) t;
+  env->SetIntField( jobj, jfid, jt );
+  
+  return true;
+}
+
+
+
+// this is only supposed to be called when the ButtonRemote
+// instance is finalized for garbage collection
+JNIEXPORT void JNICALL 
+Java_vrpn_ButtonRemote_shutdownButton( JNIEnv* env, jobject jobj )
+{
+
+  // look up where to store the button pointer
+  jclass jcls = env->GetObjectClass( jobj );
+  jfieldID jfid = env->GetFieldID( jcls, "native_button", "I" );
+  if( jfid == NULL )
+  {
+    printf( "Error in native method \"shutdownButton\":  unable to ID native button field.\n" );
+    return;
+  }
+
+  // get the button pointer
+  vrpn_Button_Remote* t = (vrpn_Button_Remote*) env->GetIntField( jobj, jfid );
+  
+  // unregister a handler and destroy the button
+  if( t > 0 )
+  {
+    t->unregister_change_handler( jobj, handle_button_change );
+    delete t;
+  }
+
+  // set the button pointer to -1
+  env->SetIntField( jobj, jfid, -1 );
+
+  // delete global reference to object (that was created in init)
+  env->DeleteGlobalRef( jobj /* is this the right object? */ );
+
+
+}
+
+
+
+
