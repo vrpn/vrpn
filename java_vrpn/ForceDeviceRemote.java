78dc46bb188c5e3ce150d9b024eefaeed72e870b
marshbur <marshbur>
2002-06-25 02:04:37 +0000

    * java_vrpn.vjp:  adds ForceDevice 	* java_vrpn.dsw:  adds ForceDeviceRemote.dsp 	* ForceDeviceRemote.dsp:  new project for force device 	* ForceDeviceRemote.java:  new 	* vrpn_ForceDeviceRemote.java:  new, partial implementation of 	native functions. 	* .cvsignore:  now ignores vrpn_ForceDeviceRemote.h (generated)

---
+++ java_vrpn/ForceDeviceRemote.java
@@ -0,0 +1,321 @@
+package vrpn;
+import java.util.*;
+
+
+
+public class ForceDeviceRemote implements Runnable
+{
+	
+	//////////////////
+	// Public structures and interfaces
+
+	public class ForceChange
+	{
+		public Date msg_time = new Date();
+		public double force[] = new double[3];
+	}
+	
+	public interface ForceChangeListener
+	{
+		public void forceUpdate( ForceChange f, ForceDeviceRemote forceDevice );
+	}
+	
+	
+	// SCP is "Surface Contact Point"
+	public class SCPChange
+	{
+		public Date msg_time = new Date( );
+		public double pos[] = new double[3];
+		public double quat[] = new double[4];
+	}
+		
+	public interface SCPChangeListener
+	{
+		public void scpUpdate( SCPChange s, ForceDeviceRemote forceDevice );
+	}
+	
+	
+	public class ForceError
+	{
+		public Date msg_time = new Date( );
+		int errorCode;
+	}
+	
+	public interface ForceErrorListener
+	{
+		public void forceError( ForceError a, ForceDeviceRemote forceDevice );
+	}
+	
+	public static final int NO_CONSTRAINT = 0;
+	public static final int POINT_CONSTRAINT = 1;
+	public static final int LINE_CONSTRAINT = 2;
+	public static final int PLANE_CONSTRAINT = 3;
+	
+	// end of the public structures and interfaces
+	//////////////////////////////////
+	
+	
+	////////////////////////////////
+	// Public methods
+	//
+	
+	/**
+	 * @exception java.lang.InstantiationException
+	 *		If the force device could not be created because of problems with
+	 *      its native code and linking.
+	 */
+	public ForceDeviceRemote( String name ) throws InstantiationException
+	{
+		try	{  this.init( name );  }
+		catch( java.lang.UnsatisfiedLinkError e )
+		{  
+			System.out.println( "Error initializing remote forece device " + name + "." );
+			System.out.println( " -- Unable to find native library." );
+			throw new InstantiationException( e.getMessage( ) );
+		}
+		
+		this.forceDeviceThread = new Thread( this );
+		this.forceDeviceThread.start( );
+	}
+	
+	
+	
+	public native void sendSurface( );
+	public native void startSurface( );
+	public native void stopSurface( );
+	public native void setVertex( int number, float x, float y, float z );
+	public native void setNormal( int number, float x, float y, float z );
+	public native void setTriangle( int number, int vertex1, int vertex2, int vertex3,
+							 int normal1, int normal2, int normal3 );
+	public native void removeTriangle( int number );
+	public native void updateTrimeshChanges( );
+	public native void setTrimeshTransform( float[] transform );
+	public native void clearTrimesh( );
+	public native void useHcollide( );
+	public native void useGhost( );
+	public native void enableConstraint( int enable );
+	public native void setConstraintMode( int mode );
+	public native void setConstraintPoint( float[] point );
+	public native void setConstraintLinePoint( float[] point );
+	public native void setConstraintLineDirection( float[] direction );
+	public native void setConstraintPlanePoint( float[] point );
+	public native void setConstraintPlaneNormal( float[] normal );
+	public native void setConstraintKSpring( float k );
+	public native void sendForceField( float[] origin, float[] force, float[][] jacobian, float radius );
+	public native void sendForceField( );
+	public native void stopForceField( );
+	
+	
+	public synchronized void addForceChangeListener( ForceChangeListener listener )
+	{
+		forceListeners.addElement( listener );
+	}
+
+	/**
+	 * @return true on success; false on failure
+	 */
+	public synchronized boolean removeForceChangeListener( ForceChangeListener listener )
+	{
+		return forceListeners.removeElement( listener );
+	}
+	
+	
+	public synchronized void addSCPChangeListener( SCPChangeListener listener )
+	{
+		scpListeners.addElement( listener );
+	}
+	
+	/**
+	 * @return true on success; false on failure
+	 */
+	public synchronized boolean removeSCPChangeListener( SCPChangeListener listener )
+	{
+		return scpListeners.removeElement( listener );
+	}
+	
+	
+	public synchronized void addForceErrorListener( ForceErrorListener listener )
+	{
+		errorListeners.addElement( listener );
+	}
+	
+	/**
+	 * @return true on success; false on failure
+	 */
+	public synchronized boolean removeForceErrorListener( ForceErrorListener listener )
+	{
+		return errorListeners.removeElement( listener );
+	}
+
+	/**
+	 * This should <b>not</b> be called by user code.
+	 */
+	public void run( )
+	{
+		while( keepForcing )
+		{
+			this.mainloop( );
+			try { Thread.sleep( 100 ); }
+			catch( InterruptedException e ) { } 
+		}
+	}
+	
+	// end public methods
+	////////////////////////
+	
+	
+	////////////////////////
+	// Protected methods
+	//
+	
+	/**
+	 * Should be called only by mainloop(), a native method which is itself
+	 * synchronized.  By extension, this method is synchronized (in that, for
+	 * a given ForceDeviceRemote object, this method can only be called from one
+	 * thread at a time).
+	 */
+	protected void handleForceChange( long tv_sec, long tv_usec,  
+									  double x, double y, double z )
+	{
+		// putting the body of this function into a synchronized block prevents
+		// other instances of ForceDeviceRemote from calling listeners' forceUpdate
+		// method concurrently.
+		synchronized( notifyingForceListenersLock )
+		{
+			ForceChange f = new ForceChange();
+			f.msg_time.setTime( tv_sec * 1000 + tv_usec );
+			f.force[0] = x;  f.force[1] = y;  f.force[2] = z;
+			
+			// notify all listeners
+			Enumeration e = forceListeners.elements( );
+			while( e.hasMoreElements( ) )
+			{
+				ForceChangeListener l = (ForceChangeListener) e.nextElement( );
+				l.forceUpdate( f, this );
+			}
+		} // end synchronized( notifyingForceListenersLock )
+	} // end method handleForceChange
+	
+	
+	/**
+	 * @see vrpn.ForceDeviceRemote.handleforceChange
+	 */
+	protected void handleSCPChange( long tv_sec, long tv_usec, 
+									double x, double y, double z, 
+									double quat0, double quat1, double quat2, double quat3 )
+	{
+		synchronized( notifyingSCPListenersLock )
+		{
+			SCPChange s = new SCPChange();
+			s.msg_time.setTime( tv_sec * 1000 + tv_usec );
+			s.pos[0] = x;  s.pos[1] = y;  s.pos[2] = z;
+			s.quat[0] = quat0;  s.quat[1] = quat1;  s.quat[2] = quat2;  
+			s.quat[3] = quat3;
+			
+			// notify all listeners
+			Enumeration e = scpListeners.elements( );
+			while( e.hasMoreElements( ) )
+			{
+				SCPChangeListener l = (SCPChangeListener) e.nextElement( );
+				l.scpUpdate( s, this );
+			}
+		} // end synchronized( notifyingSCPListenersLock )
+	} // end method handleSCPChange
+		
+	
+	/**
+	 * @see vrpn.ForceDeviceRemote.handleForceChange
+	 */
+	protected void handleForceError( long tv_sec, long tv_usec, int errorCode )
+	{
+		synchronized( notifyingErrorListenersLock )
+		{
+			ForceError u = new ForceError();
+			u.msg_time.setTime( tv_sec * 1000 + tv_usec );
+			u.errorCode = errorCode;
+			
+			// notify all listeners
+			Enumeration e = errorListeners.elements( );
+			while( e.hasMoreElements( ) )
+			{
+				ForceErrorListener l = (ForceErrorListener) e.nextElement( );
+				l.forceError( u, this );
+			}
+		} // end synchronized( notifyingAccelerationListenersLock )
+	} // end method handleErrorChange
+	
+
+	/**
+	 * Initialize the native force device object
+	 * @param name The name of the force device and host (e.g., <code>"Phantom0@myhost.edu"</code>).
+	 * @return <code>true</code> if the force device was connected successfully, 
+	 *			<code>false</code> otherwise.
+	 */
+	protected native boolean init( String name );
+
+	/**
+	 * This should only be called from the method finalize()
+	 */
+	protected native void shutdownForceDevice( );
+	
+	protected synchronized native void mainloop( );
+	
+	protected void finalize( ) throws Throwable
+	{
+		keepForcing = false;
+		while( forceDeviceThread.isAlive( ) )
+		{
+			try { forceDeviceThread.join( ); }
+			catch( InterruptedException e ) { }
+		}
+		forceListeners.removeAllElements( );
+		scpListeners.removeAllElements( );
+		errorListeners.removeAllElements( );
+		this.shutdownForceDevice( );
+	}
+	
+	// end protected methods
+	///////////////////////
+	
+	///////////////////
+	// data members
+	
+	// this is used by the native code to store a C++ pointer to the 
+	// native vrpn_ForceDeviceRemote object
+	protected int native_force_device = -1;
+	
+	// this is used to stop and to keep running the force device thread
+	// in an orderly fashion.
+	protected boolean keepForcing = true;
+	
+	// the force device thread
+	Thread forceDeviceThread = null;
+	
+	protected Vector forceListeners = new Vector( );
+	protected Vector scpListeners = new Vector( );
+	protected Vector errorListeners = new Vector( );
+	
+	/**
+	 * these notifying*ListenersLock variables are used to ensure that multiple
+	 * ForceDeviceRemote objects running in multiple threads don't call the 
+	 * forceUpdate, et. al., method of some single object concurrently.
+	 * For example, the handleForceChange(...) method, which is invoked from native 
+	 * code, gets a lock on the notifyingForceListenersLock object.  Since that object
+	 * is static, all other instances of ForceDeviceRemote must wait before notifying 
+	 * their listeners and completing their handleForceChange(...) methods.
+	 * They are necessary, in part, because methods in an interface can't be declared
+	 * synchronized (and the semantics of the keyword 'synchronized' aren't what's
+	 * wanted here, anyway -- we want synchronization across all instances, not just a 
+	 * single object).
+	 */
+	protected final static Object notifyingForceListenersLock = new Object( );
+	protected final static Object notifyingSCPListenersLock = new Object( );
+	protected final static Object notifyingErrorListenersLock = new Object( );
+	
+	// static initialization
+	static 
+	{
+		System.loadLibrary( "ForceDeviceRemote" );
+	}
+	
+}
---
+++ java_vrpn/vrpn_ForceDeviceRemote.cpp
@@ -0,0 +1,586 @@
+
+#include "stdio.h"
+#include "jni.h"
+#include "vrpn_ForceDevice.h"
+
+#include "java_vrpn.h"
+#include "vrpn_ForceDeviceRemote.h"
+
+
+JavaVM* jvm = NULL;
+jclass jclass_vrpn_ForceDeviceRemote = NULL;
+jfieldID jfid_vrpn_ForceDeviceRemote_native_force_device = NULL;
+
+
+//////////////////////////
+//  DLL functions
+
+
+// This is called when the Java Virtual Machine loads this library
+//  and sets some global references that are used elsewhere.
+JNIEXPORT jint JNICALL JNI_OnLoad( JavaVM* jvm, void* reserved )
+{
+  //////////////
+  // set the jvm reference
+  ::jvm = jvm;
+
+  ///////////////
+  // get the JNIEnv pointer
+  JNIEnv* env;
+  if( jvm->GetEnv( (void**) &env, JAVA_VRPN_JNI_VERSION ) != JNI_OK )
+  {
+    printf( "Error loading vrpn ForceDeviceRemote native library.\n" );
+    return JNI_ERR;
+  }
+  
+  ///////////////
+  // get the jclass reference to vrpn.ForceDeviceRemote
+  jclass cls = env->FindClass( "vrpn/ForceDeviceRemote" );
+  if( cls == NULL )
+  {
+    printf( "Error loading vrpn ForceDeviceRemote native library "
+            "while trying to find class vrpn.ForceDeviceRemote.\n" );
+    return JNI_ERR;
+  }
+
+  // make a global reference so the class can be referenced later.
+  // this must be a _WEAK_ global reference, so the class will be
+  // garbage collected, and so the JNI_OnUnLoad handler will be called
+  jclass_vrpn_ForceDeviceRemote = (jclass) env->NewWeakGlobalRef( cls );
+  if( jclass_vrpn_ForceDeviceRemote == NULL )
+  {
+    printf( "Error loading vrpn ForceDeviceRemote native library "
+            "while setting up class vrpn.ForceDeviceRemote.\n" );
+    return JNI_ERR;
+  }
+
+  ////////////////
+  // get a jfid field id reference to the "native_force_device" 
+  // field of class vrpn.ForceDeviceRemote.
+  // field ids do not have to be made into global references.
+  jfid_vrpn_ForceDeviceRemote_native_force_device 
+    = env->GetFieldID( jclass_vrpn_ForceDeviceRemote, "native_force_device", "I" );
+  if( jfid_vrpn_ForceDeviceRemote_native_force_device == NULL )
+  {
+    printf( "Error loading vrpn ForceDeviceRemote native library "
+            "while looking into class vrpn.ForceDeviceRemote.\n" );
+    return JNI_ERR;
+  }
+  
+  return JAVA_VRPN_JNI_VERSION;
+} // end JNI_OnLoad
+
+
+
+JNIEXPORT void JNICALL JNI_OnUnload( JavaVM* jvm, void* reserved )
+{
+  // get the JNIEnv pointer
+  JNIEnv* env;
+  if( jvm->GetEnv( (void**) &env, JAVA_VRPN_JNI_VERSION ) != JNI_OK )
+  {
+    printf( "Error unloading vrpn ForceDeviceRemote native library.\n" );
+    return;
+  }
+
+  // delete the global reference to the vrpn.ForceDeviceRemote class
+  env->DeleteWeakGlobalRef( jclass_vrpn_ForceDeviceRemote );
+}
+
+// end DLL functions
+/////////////////////////////
+
+
+/////////////////////////////
+// java_vrpn utility funcitons
+
+void handle_force_change( void* userdata, const vrpn_FORCECB info )
+{
+  if( jvm == NULL )
+  {
+    printf( "Error in handle_force_change:  uninitialized jvm.\n" );
+    return;
+  }
+
+  /*
+  printf( "force change (C):  time:  %d.%d;\n"
+          "\tforce: %f %f %f;\n",
+          info.msg_time.tv_sec, info.msg_time.tv_usec,
+          info.force[0], info.force[1], info.force[2] );
+  */
+  
+  JNIEnv* env;
+  jvm->AttachCurrentThread( (void**) &env, NULL );
+  
+  jobject jobj = (jobject) userdata;
+  jclass jcls = env->GetObjectClass( jobj );
+  jmethodID jmid = env->GetMethodID( jcls, "handleForceChange", "(JJDDD)V" );
+  if( jmid == NULL )
+  {
+    printf( "Warning:  vrpn_ForceDeviceRemote library was unable to find the "
+            "Java method \'handleForceChange\'.  This may indicate a version mismatch.\n" );
+    return;
+  }
+  env->CallVoidMethod( jobj, jmid, (jlong) info.msg_time.tv_sec, (jlong) info.msg_time.tv_usec,
+                       (jdouble) info.force[0], (jdouble) info.force[1], (jdouble) info.force[2] );
+
+} // end handle_force_change
+
+
+void handle_scp_change( void* userdata, const vrpn_FORCESCPCB info )
+{
+  if( jvm == NULL )
+  {
+    printf( "Error in handle_scp_change:  uninitialized jvm.\n" );
+    return;
+  }
+
+  /*
+  printf( "scp change (C):  time:  %d.%d;\n"
+          "\pos: %f %f %f;\n\tquat:  %f %f %f\n",
+          info.msg_time.tv_sec, info.msg_time.tv_usec,
+          info.pos[0], info.pos[1], info.pos[2],
+          info.quat[0], info.quat[1], info.quat[2], info.quat[3] );
+  */
+  
+  JNIEnv* env;
+  jvm->AttachCurrentThread( (void**) &env, NULL );
+  
+  jobject jobj = (jobject) userdata;
+  jclass jcls = env->GetObjectClass( jobj );
+  jmethodID jmid = env->GetMethodID( jcls, "handleSCPChange", "(JJDDDDDDD)V" );
+  if( jmid == NULL )
+  {
+    printf( "Warning:  vrpn_ForceDeviceRemote library was unable to find the "
+            "Java method \'handleSCPChange\'.  This may indicate a version mismatch.\n" );
+    return;
+  }
+  env->CallVoidMethod( jobj, jmid, (jlong) info.msg_time.tv_sec, (jlong) info.msg_time.tv_usec,
+                       (jdouble) info.pos[0], (jdouble) info.pos[1], (jdouble) info.pos[2],
+                       (jdouble) info.quat[0], (jdouble) info.quat[1], (jdouble) info.quat[2], 
+                       (jdouble) info.quat[3] );
+
+} // end handle_scp_change
+
+
+void handle_force_error( void* userdata, const vrpn_FORCEERRORCB info )
+{
+  if( jvm == NULL )
+  {
+    printf( "Error in handle_force_error:  uninitialized jvm.\n" );
+    return;
+  }
+
+  /*
+  printf( "force error (C):  time:  %d.%d;  force: %d;\n",
+          info.msg_time.tv_sec, info.msg_time.tv_usec,
+          info.error_code );
+  */
+  
+  JNIEnv* env;
+  jvm->AttachCurrentThread( (void**) &env, NULL );
+  
+  jobject jobj = (jobject) userdata;
+  jclass jcls = env->GetObjectClass( jobj );
+  jmethodID jmid = env->GetMethodID( jcls, "handleForceError", "(JJDDI)V" );
+  if( jmid == NULL )
+  {
+    printf( "Warning:  vrpn_ForceDeviceRemote library was unable to find the "
+            "Java method \'handleForceError\'.  This may indicate a version mismatch.\n" );
+    return;
+  }
+  env->CallVoidMethod( jobj, jmid, (jlong) info.msg_time.tv_sec, (jlong) info.msg_time.tv_usec,
+                       (jint) info.error_code );
+
+} // end handle_force_error]
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    init
+ * Signature: (Ljava/lang/String;)Z
+ */
+JNIEXPORT jboolean JNICALL 
+Java_vrpn_ForceDeviceRemote_init( JNIEnv* env, jobject jobj, jstring jname )
+{
+  printf( "in Java_ForceDeviceRemote_init(...)\n" );
+
+  // look up where to store the force device pointer
+  jclass jcls = env->GetObjectClass( jobj );
+  jfieldID jfid = env->GetFieldID( jcls, "native_force_device", "I" );
+  if( jfid == NULL )
+  {
+    printf( "Error in native method \"init\":  unable to ID native force device field.\n" );
+    return false;
+  }
+
+  // make a global reference to the Java ForceDeviceRemote
+  // object, so that it can be referenced in the functions
+  // handle_*_change(...)
+  jobj = env->NewGlobalRef( jobj );
+
+  // create the force device
+  const char* name = env->GetStringUTFChars( jname, NULL );
+  vrpn_ForceDevice_Remote* f = new vrpn_ForceDevice_Remote( name );
+  f->register_force_change_handler( jobj, handle_force_change );
+  f->register_scp_change_handler( jobj, handle_scp_change );
+  f->register_error_handler( jobj, handle_force_error );
+  env->ReleaseStringUTFChars( jname, name );
+  
+  // now stash 'f' in the jobj's 'native_force_device' field
+  jint jf = (jint) f;
+  env->SetIntField( jobj, jfid, jf );
+  
+  return true;
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    mainloop
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL 
+Java_vrpn_ForceDeviceRemote_mainloop( JNIEnv* env, jobject jobj )
+{
+  // look up the force device pointer
+  jclass jcls = env->GetObjectClass( jobj );
+  jfieldID jfid = env->GetFieldID( jcls, "native_force_device", "I" );
+  if( jfid == NULL )
+  {
+    printf( "Error in native method \"mainloop\":  unable to ID native force device field.\n" );
+    return;
+  }
+  vrpn_ForceDevice_Remote* f = (vrpn_ForceDevice_Remote*) env->GetIntField( jobj, jfid );
+  if( f <= 0 )  // this force device is uninitialized or has been shut down already
+    return;
+
+  // now call mainloop
+  f->mainloop( );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    shutdownForceDevice
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL 
+Java_vrpn_ForceDeviceRemote_shutdownForceDevice( JNIEnv* env, jobject jobj )
+{
+  // look up where to store the force device pointer
+  jclass jcls = env->GetObjectClass( jobj );
+  jfieldID jfid = env->GetFieldID( jcls, "native_force_device", "I" );
+  if( jfid == NULL )
+  {
+    printf( "Error in native method \"shutdownForceDevice\":  unable to ID native force device field.\n" );
+    return;
+  }
+
+  // get the force deivce pointer
+  vrpn_ForceDevice_Remote* f = (vrpn_ForceDevice_Remote*) env->GetIntField( jobj, jfid );
+  
+  // unregister a handler and destroy the force device
+  if( f > 0 )
+  {
+    f->unregister_force_change_handler( jobj, handle_force_change );
+    f->unregister_scp_change_handler( jobj, handle_scp_change );
+    f->unregister_error_handler( jobj, handle_force_error );
+    delete f;
+  }
+
+  // set the force device pointer to -1
+  env->SetIntField( jobj, jfid, -1 );
+
+  // delete global reference to object (that was created in init)
+  env->DeleteGlobalRef( jobj );
+
+}
+
+
+// end java_vrpn utility functions
+////////////////////////////////////
+
+
+////////////////////////////////////
+// native functions from java class ForceDeviceRemote
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setConstraintMode
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setConstraintMode
+  (JNIEnv *, jobject, jint)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintMode is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setConstraintPoint
+ * Signature: ([F)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setConstraintPoint
+  (JNIEnv *, jobject, jfloatArray)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintPoint is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setConstraintLinePoint
+ * Signature: ([F)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setConstraintLinePoint
+  (JNIEnv *, jobject, jfloatArray)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintLinePoint is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setConstraintLineDirection
+ * Signature: ([F)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setConstraintLineDirection
+  (JNIEnv *, jobject, jfloatArray)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintLineDirection is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setConstraintPlaneNormal
+ * Signature: ([F)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setConstraintPlaneNormal
+  (JNIEnv *, jobject, jfloatArray)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintPlaneNormal is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setConstraintPlanePoint
+ * Signature: ([F)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setConstraintPlanePoint
+  (JNIEnv *, jobject, jfloatArray)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintPlanePoint is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setConstraintKSpring
+ * Signature: (F)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setConstraintKSpring
+  (JNIEnv *, jobject, jfloat)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintKSpring is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setTriangle
+ * Signature: (IIIIIII)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setTriangle
+  (JNIEnv *, jobject, jint, jint, jint, jint, jint, jint, jint)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setTriangle is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    useHcollide
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_useHcollide
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_useHcollide is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setTrimeshTransform
+ * Signature: ([F)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setTrimeshTransform
+  (JNIEnv *, jobject, jfloatArray)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setTrimeshTransform is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    updateTrimeshChanges
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_updateTrimeshChanges
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_updateTrimeshChanges is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    clearTrimesh
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_clearTrimesh
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_clearTrimesh is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    sendForceField
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_sendForceField__
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_sendForceField__ is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    stopForceField
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_stopForceField
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_stopForceField is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    sendForceField
+ * Signature: ([F[F[[FF)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_sendForceField___3F_3F_3_3FF
+  (JNIEnv *, jobject, jfloatArray, jfloatArray, jobjectArray, jfloat)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_sendForceField___3F_3F_3_3FF is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    sendSurface
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_sendSurface
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_sendSurface is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    startSurface
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_startSurface
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_startSurface is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    stopSurface
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_stopSurface
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_stopSurface is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setVertex
+ * Signature: (IFFF)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setVertex
+  (JNIEnv *, jobject, jint, jfloat, jfloat, jfloat)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setVertex is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    useGhost
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_useGhost
+  (JNIEnv *, jobject)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_useGhost is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    setNormal
+ * Signature: (IFFF)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_setNormal
+  (JNIEnv *, jobject, jint, jfloat, jfloat, jfloat)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setNormal is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    removeTriangle
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_removeTriangle
+  (JNIEnv *, jobject, jint)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_setConstraintMode is not implemented.\n" );
+}
+
+
+/*
+ * Class:     vrpn_ForceDeviceRemote
+ * Method:    enableConstraint
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_vrpn_ForceDeviceRemote_enableConstraint
+  (JNIEnv *, jobject, jint)
+{
+  printf( "Function Java_vrpn_ForceDeviceRemote_enableConstraint is not implemented.\n" );
+}
+
+
+// end native functions for java class ForceDeviceRemote
+///////////////////////////////////////////
+
+
