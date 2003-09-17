253fb0621fe51147e3b8178e52ae98446ccf015a
taylorr <taylorr>
2003-09-17 01:02:38 +0000

    2003-09-16  Blair MacIntyre     <blair@cc.gatech.edu>
            * vrpn_Tracker_isense.h : Added code to add Wands and other
                    devices to the tracker like the Fastrak version of
                    the Intersense driver could.
            * vrpn_Tracker_isense.C : Same.

--- vrpn_Tracker_isense.h
+++ vrpn_Tracker_isense.h
@@ -25,10 +25,22 @@ class vrpn_Tracker_InterSense : public vrpn_Tracker {
 
   vrpn_Tracker_InterSense(const char *name, 
                           vrpn_Connection *c,
-                          int commPort);
+                          int commPort, const char *additional_reset_commands = NULL,
+					      int is900_timestamps = 0);
 
   ~vrpn_Tracker_InterSense();
 
+  /// Add an IS900 button device to one of the sensors
+  /// This allows configuration of an InterSense IS-900
+  int	add_is900_button(const char *button_device_name, int sensor, int numbuttons = 5);
+
+  /// Add the analog part of an IS900 joystick device to one of the sensors
+  /// This allows configuration of an InterSense IS-900
+  /// The optional parameters specify the clipping and scaling to take the reports
+  /// from the two joystick axes into the range [-1..1].  The default is unscaled.
+  int	add_is900_analog(const char *analog_device_name, int sensor,
+	    double c0Min = -1, double c0Low = 0, double c0Hi = 0, double c0Max = 1,
+	    double c1Min = -1, double c1Low = 0, double c1Hi = 0, double c1Max = 1);
 
   /// This function should be called each time through the main loop
   /// of the server code. It polls for a report from the tracker and
@@ -41,6 +53,9 @@ class vrpn_Tracker_InterSense : public vrpn_Tracker {
   
   virtual void get_report(void);
   virtual void reset();
+  virtual void send_report(void);
+
+  char	add_reset_cmd[2048];	//< Additional reset commands to be sent
 
   int m_CommPort;
 #ifdef VRPN_INCLUDE_INTERSENSE
@@ -48,6 +63,16 @@ class vrpn_Tracker_InterSense : public vrpn_Tracker {
   ISD_TRACKER_INFO_TYPE m_TrackerInfo;
   ISD_STATION_INFO_TYPE m_StationInfo[ISD_MAX_STATIONS];
 #endif
+
+  int	do_is900_timestamps;	    //< Request and process IS-900 timestamps?
+  struct timeval is900_zerotime;    //< When the IS-900 time counter was zeroed
+  vrpn_Button_Server		*is900_buttons[ISD_MAX_STATIONS];	//< Pointer to button on each sensor (NULL if none)
+  vrpn_Clipping_Analog_Server	*is900_analogs[ISD_MAX_STATIONS];	//< Pointer to analog on each sensor (NULL if none)
+
+  /// Augments the basic Fastrak format to include IS900 features if needed
+  int	set_sensor_output_format(int sensor);
+
+  void  getTrackerInfo(char *msg);
 };
 
 
