8a8c91fe6991e4988e4601af554729b85be5f76c
taylorr <taylorr>
2004-12-07 20:21:17 +0000

    Added comment to ChangeLog.
    Put the other two files back into a state usable by others.

--- vrpn_Configure.h
+++ vrpn_Configure.h
@@ -64,7 +64,7 @@
 // server, using phantom as a common device, and phantom
 // configuration in .cfg file.
 // PLEASE SPECIFY PATH TO GHOSTLIB IN NEXT SECTION IF YOU USE THIS
-#define     VRPN_USE_PHANTOM_SERVER
+//#define     VRPN_USE_PHANTOM_SERVER
 
 //------------------------
 // Instructs vrpn to use SensAble's HDAPI rather than GHOST library.
@@ -116,7 +116,7 @@
 // the DirectX SDK (from its standard installation in C:\DXSDK).
 // Later in this file, we also instruct the compiler to link with
 // the DirectX library if this is defined.
-#define	VRPN_USE_DIRECTINPUT
+//#define	VRPN_USE_DIRECTINPUT
 
 //-----------------------
 // Instructs the VRPN server to create an entry for the Adrienne
@@ -143,7 +143,7 @@
 // the National Instruments Nidaq libary to control analog outputa.
 // Later in this file, we also instruct the compiler to link with
 // the National Instruments libraries if this is defined.
-#define	VRPN_USE_NATIONAL_INSTRUMENTS
+//#define	VRPN_USE_NATIONAL_INSTRUMENTS
 
 //---------------------------------------------------------------//
 // DO NOT EDIT BELOW THIS LINE FOR NORMAL CONFIGURATION SETTING. //
