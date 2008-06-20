4af3c93c6c60ae4d2465c523c86f132f43901af9
marshbur <marshbur>
2008-06-20 16:35:16 +0000

    * PoserRemote.java:  added relative-pose methods.

--- java_vrpn/vrpn/PoserRemote.java
+++ java_vrpn/vrpn/PoserRemote.java
@@ -52,22 +52,91 @@ public class PoserRemote extends VRPNDevice implements Runnable
 		return retval;
 	}
 	
-	
-	public boolean requestPoseVelocity( double[] position,  double[] quaternion,  double interval )
+	/**
+	 * @param positionDelta new position = old position + positionDelta
+	 * @param quaternion new orientation = quaternion * old orientation
+	 * @return true if the request was successfully transmitted to the poser.
+	 * false if there was some problems sending the request.
+	 */
+	public boolean requestPoseRelative( double[] positionDelta, double[] quaternion )
 	{
-		return requestPoseVelocity( new Date(), position,  quaternion,  interval  );
+		return requestPoseRelative( new Date(), positionDelta, quaternion );
+	}
+	
+	/**
+	 * 
+	 * @param t the time at which this pose should be adopted
+	 * @param positionDelta new position = old position + positionDelta
+	 * @param quaternion new orientation = quaternion * old orientation
+	 * @return true if the request was successfully transmitted to the poser.
+	 * false if there was some problems sending the request.
+	 */
+	public boolean requestPoseRelative( Date t, double[] positionDelta, double[] quaternion )
+	{
+		boolean retval = false;
+		if( positionDelta.length != 3 || quaternion.length != 4 )
+			return false;
+		long msecs = t.getTime( );
+		long secs = msecs / 1000;
+		synchronized( downInVrpnLock )
+		{
+			retval = requestPose_native( secs, ( msecs - secs * 1000 ) * 1000, positionDelta, quaternion );
+		}
+		return retval;
 	}
 	
 	
-	public boolean requestPoseVelocity( Date t,  double[] position,  double[] quaternion,  double interval )
+	public boolean requestVelocity( double[] velocity,  double[] quaternion,  double interval )
+	{
+		return requestVelocity( new Date(), velocity,  quaternion,  interval  );
+	}
+	
+	
+	public boolean requestVelocity( Date t,  double[] velocity,  double[] quaternion,  double interval )
 	{
 		boolean retval = false;
-		if( position.length != 3 || quaternion.length != 4 )
-			return false;
-		
+		if( velocity.length != 3 || quaternion.length != 4 )
+			return false;		
+		long msecs = t.getTime( );
+		long secs = msecs / 1000;
 		synchronized( downInVrpnLock )
 		{
-			retval = requestPoseVelocity_native( t.getTime(), t.getTime(), position, quaternion, interval );
+			retval = requestVelocity_native( secs, ( msecs - secs * 1000 ) * 1000, velocity, quaternion, interval );
+		}
+		return retval;
+	}
+	
+	/**
+	 * @param velocityDelta new velocity = old velocity + velocityDelta
+	 * @param quaternion new orientation = quaternion * old orientation
+	 * @param intervalDelta new interval = old interval + intervalDelta
+	 * @return true if the request was successfully transmitted to the poser.
+	 * false if there was some problems sending the request.
+	 */
+	public boolean requestVelocityRelative( double[] velocityDelta, double[] quaternion, double intervalDelta )
+	{
+		return requestVelocityRelative( new Date(), velocityDelta, quaternion, intervalDelta );
+	}
+	
+	
+	/**
+	 * @param t the time at which this velocity pose should be adopted.
+	 * @param velocityDelta new velocity = old velocity + velocityDelta
+	 * @param quaternion new orientation = quaternion * old orientation
+	 * @param intervalDelta new interval = old interval + intervalDelta
+	 * @return true if the request was successfully transmitted to the poser.
+	 * false if there was some problems sending the request.
+	 */
+	public boolean requestVelocityRelative( Date t, double[] velocityDelta, double[] quaternion, double intervalDelta )
+	{
+		boolean retval = false;
+		if( velocityDelta.length != 3 || quaternion.length != 4 )
+			return false;
+		long msecs = t.getTime( );
+		long secs = msecs / 1000;
+		synchronized( downInVrpnLock )
+		{
+			retval = requestVelocityRelative_native( secs, ( msecs - secs * 1000 ) * 1000, velocityDelta, quaternion, intervalDelta );
 		}
 		return retval;
 	}
@@ -108,7 +177,9 @@ public class PoserRemote extends VRPNDevice implements Runnable
 	
 	protected native void mainloop( );
 	protected native boolean requestPose_native( long secs, long usecs, double[] position, double[] quaternion );
-	protected native boolean requestPoseVelocity_native( long secs, long usecs, double[] position, double[] quaternion, double interval);
+	protected native boolean requestPoseRelative_native( long secs, long usecs, double[] position_delta, double[] quaternion );
+	protected native boolean requestVelocity_native( long secs, long usecs, double[] position, double[] quaternion, double interval);
+	protected native boolean requestVelocityRelative_native( long secs, long usecs, double[] velocity_delta, double[] quaternion, double interval_delta );
 	
 	// end protected methods
 	///////////////////////
