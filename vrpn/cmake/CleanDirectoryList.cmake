ebf3c626d7af11d076223d13daf62c2f6cf492a3
Ryan Pavlik <rpavlik@iastate.edu>
2009-12-07 12:13:46 -0600

    Bring in the new hotness

---
+++ vrpn/cmake/CleanDirectoryList.cmake
@@ -0,0 +1,19 @@
+# - Removes duplicate entries and non-directories from a provided list
+#
+# Original Author:
+# 2009 Ryan Pavlik <rpavlik@iastate.edu>
+# http://academic.cleardefinition.com
+# Iowa State University HCI Graduate Program/VRAC
+
+function(clean_directory_list var)
+	set(_in ${ARGN})
+	list(SORT _in)
+	list(REMOVE_DUPLICATES _in)
+	set(_out)
+	foreach(_dir ${_in})
+		if(IS_DIRECTORY "${_dir}")
+			list(APPEND _out "${_dir}")
+		endif()
+	endforeach()
+	set(${var} "${_out}" PARENT_SCOPE)
+endfunction()
