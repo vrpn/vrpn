e78923772741f85f7b18ebad3bc8f750db503d16
Ryan Pavlik <rpavlik@iastate.edu>
2010-01-28 10:25:41 -0600

    More work on the cmake modules, ported from Sharp-2

---
+++ vrpn/cmake/MSVCMultipleProcessCompile.cmake
@@ -0,0 +1,21 @@
+# - Compile with multiple processes on MSVC
+#
+#  include(MSVCMultipleProcessCompile)
+#
+# Requires these CMake modules:
+#  ListCombinations.cmake
+#
+# Original Author:
+# 2009-2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
+# http://academic.cleardefinition.com
+# Iowa State University HCI Graduate Program/VRAC
+
+if(MSVC)
+	string(TOUPPER "${CMAKE_CONFIGURATION_TYPES}" _conftypesUC)
+	include(ListCombinations)
+	list_combinations(_varnames PREFIXES CMAKE_C_FLAGS_ CMAKE_CXX_FLAGS_
+		SUFFIXES ${_conftypesUC})
+	foreach(_var ${_varnames})
+		set(${_var} "${${_var}} /MP")
+	endforeach()
+endif()
