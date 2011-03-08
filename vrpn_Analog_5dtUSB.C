29caf71ac8ed184d18329900bbc0ea85712b3acc
Ryan Pavlik <rpavlik@iastate.edu>
2011-03-08 10:01:22 -0600

    fix mask for glove handedness

--- vrpn_Analog_5dtUSB.C
+++ vrpn_Analog_5dtUSB.C
@@ -11,7 +11,7 @@
 // USB vendor and product IDs for the models we support
 static const vrpn_uint16 vrpn_5DT_VENDOR = 0x5d70;
 
-static const vrpn_uint16 vrpn_5DT_RIGHT_MASK = 0x0000;
+static const vrpn_uint16 vrpn_5DT_LEFT_MASK = 0x0001;
 static const vrpn_uint16 vrpn_5DT_RIGHT = 0x0000;
 static const vrpn_uint16 vrpn_5DT_LEFT = 0x0001;
 static const vrpn_uint16 vrpn_5DT_WIRELESS_PORTA_MASK = 0x0002;
@@ -112,28 +112,28 @@ void vrpn_Analog_5dtUSB::report(vrpn_uint32 class_of_service) {
 
 
 vrpn_Analog_5dtUSB_Glove5Left::vrpn_Analog_5dtUSB_Glove5Left(const char *name, vrpn_Connection *c) :
-	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_RIGHT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_LEFT | vrpn_5DT_DATAGLOVE5),
+	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_LEFT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_LEFT | vrpn_5DT_DATAGLOVE5),
 	                   5,
 	                   true,
 	                   name,
 	                   c) { }
 
 vrpn_Analog_5dtUSB_Glove5Right::vrpn_Analog_5dtUSB_Glove5Right(const char *name, vrpn_Connection *c) :
-	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_RIGHT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_RIGHT | vrpn_5DT_DATAGLOVE5),
+	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_LEFT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_RIGHT | vrpn_5DT_DATAGLOVE5),
 	                   5,
 	                   false,
 	                   name,
 	                   c) { }
 
 vrpn_Analog_5dtUSB_Glove14Left::vrpn_Analog_5dtUSB_Glove14Left(const char *name, vrpn_Connection *c) :
-	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_RIGHT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_LEFT | vrpn_5DT_DATAGLOVE14),
+	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_LEFT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_LEFT | vrpn_5DT_DATAGLOVE14),
 	                   14,
 	                   true,
 	                   name,
 	                   c) { }
 
 vrpn_Analog_5dtUSB_Glove14Right::vrpn_Analog_5dtUSB_Glove14Right(const char *name, vrpn_Connection *c) :
-	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_RIGHT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_RIGHT | vrpn_5DT_DATAGLOVE14),
+	vrpn_Analog_5dtUSB(new vrpn_HidProductMaskAcceptor(vrpn_5DT_VENDOR, vrpn_5DT_LEFT_MASK | vrpn_5DT_DATAGLOVE5_MASK, vrpn_5DT_RIGHT | vrpn_5DT_DATAGLOVE14),
 	                   14,
 	                   false,
 	                   name,
