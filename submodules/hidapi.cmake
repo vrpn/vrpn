b242a702e32a31502d001c221adec76ba52c3eab
Ryan Pavlik <rpavlik@iastate.edu>
2011-09-01 13:48:24 -0500

    be more precise in dependencies for platforms for hidapi

--- submodules/hidapi.cmake
+++ submodules/hidapi.cmake
@@ -11,14 +11,12 @@ elseif(APPLE)
 	find_package(MacHID)
 	set(HIDAPI_DEPS_CHECK MACHID_FOUND)
 
-else()
+elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
 	find_package(Libusb1)
 	set(HIDAPI_DEPS_CHECK LIBUSB1_FOUND)
 endif()
 
 
-if(CMAKE_SYSTEM_NAME MATCHES "Linux")
-	find_library(HIDAPI_LIBRT rt)
 endif()
 
 if(EXISTS "${VRPN_SOURCE_DIR}/submodules/hidapi/hidapi/hidapi.h")
@@ -35,24 +33,27 @@ option_requires(VRPN_USE_LOCAL_HIDAPI
 if(VRPN_USE_LOCAL_HIDAPI)
 	set(HIDAPI_INCLUDE_DIRS "${CMAKE_CURRENT_SOURCE_DIR}/hidapi/hidapi")
 	set(HIDAPI_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/hidapi/hidapi/hidapi.h")
+	set(HIDAPI_FOUND TRUE)
 	if(APPLE)
 		list(APPEND HIDAPI_SOURCES "${CMAKE_CURRENT_SOURCE_DIR}/hidapi/mac/hid.c")
 		set(HIDAPI_LIBRARIES ${MACHID_LIBRARIES})
-		
+
 	elseif(WIN32)
 		list(APPEND HIDAPI_SOURCES "${PROJECT_SOURCE_DIR}/vrpn_Local_HIDAPI.C")
 		set(HIDAPI_LIBRARIES ${WINHID_LIBRARIES} setupapi)
-		
+
+	elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
+			list(APPEND HIDAPI_SOURCES "${PROJECT_SOURCE_DIR}/vrpn_HIDAPI_Linux_Hack.c")
+			set(HIDAPI_LIBRARIES ${LIBUSB1_LIBRARIES})
+			list(APPEND HIDAPI_INCLUDE_DIRS ${LIBUSB1_INCLUDE_DIRS})
+		find_library(HIDAPI_LIBRT rt)
+		if(HIDAPI_LIBRT)
+			mark_as_advanced(HIDAPI_LIBRT)
+			list(APPEND HIDAPI_LIBRARIES ${HIDAPI_LIBRT})
+		endif()
 	else()
-		list(APPEND HIDAPI_SOURCES "${PROJECT_SOURCE_DIR}/vrpn_HIDAPI_Linux_Hack.c")
-		set(HIDAPI_LIBRARIES ${LIBUSB1_LIBRARIES})
-		list(APPEND HIDAPI_INCLUDE_DIRS ${LIBUSB1_INCLUDE_DIRS})
-	endif()
-	if(HIDAPI_LIBRT)
-		mark_as_advanced(HIDAPI_LIBRT)
-		list(APPEND HIDAPI_LIBRARIES ${HIDAPI_LIBRT})
+		set(HIDAPI_FOUND FALSE)
 	endif()
 
 	source_group("HIDAPI Submodule" FILES ${HIDAPI_SOURCES})
-	set(HIDAPI_FOUND TRUE)
 endif()
