cea63ef0360c9ce52d1c244373b497e75a9b72b9
Ryan Pavlik <rpavlik@iastate.edu>
2012-07-06 11:32:53 -0500

    Add class representing a SerialPort (wrapping vrpn_Serial routines)

---
+++ vrpn_SerialPort.C
@@ -0,0 +1,181 @@
+/**
+	@file
+	@brief Implementation
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Internal Includes
+#include "vrpn_SerialPort.h"
+#include "vrpn_Serial.h"
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <stdio.h>
+#include <vector>
+#include <exception>
+#include <algorithm>
+
+typedef std::vector<unsigned char> DynamicBufferType;
+
+vrpn_SerialPort::vrpn_SerialPort(const char * portname, long baud, int charsize, vrpn_SER_PARITY parity)
+	: _comm(vrpn_open_commport(portname, baud, charsize, parity))
+	, _rts_status(false) {
+	if (!is_open()) {
+		throw OpenFailure();
+	}
+}
+
+
+vrpn_SerialPort::vrpn_SerialPort()
+	: _comm(-1)
+	, _rts_status(false) {}
+
+vrpn_SerialPort::~vrpn_SerialPort() {
+	if (is_open()) {
+		try {
+			close();
+		} catch (std::exception &) {
+			fprintf(stderr, "Error when closing serial port in destructor.\n");
+		}
+	}
+}
+
+void vrpn_SerialPort::open(const char * portname, long baud, int charsize, vrpn_SER_PARITY parity) {
+	if (is_open()) {
+		throw AlreadyOpen();
+	}
+	_comm = vrpn_open_commport(portname, baud, charsize, parity);
+	if (!is_open()) {
+
+		throw OpenFailure();
+	}
+}
+
+void vrpn_SerialPort::close() {
+	requiresOpen();
+	int ret = vrpn_close_commport(_comm);
+	if (ret != 0) {
+		throw CloseFailure();
+	}
+}
+
+int vrpn_SerialPort::write(std::string const& buffer) {
+	if (buffer.size() > 0) {
+		DynamicBufferType buf(buffer.c_str(), buffer.c_str() + buffer.size());
+		return write(&(buf[0]), buffer.size());
+	}
+	return 0;
+}
+
+int vrpn_SerialPort::write(const unsigned char * buffer, int bytes) {
+	requiresOpen();
+	int ret = vrpn_write_characters(_comm, buffer, bytes);
+	if (ret = -1) {
+		throw WriteFailure();
+	}
+	return ret;
+}
+
+int vrpn_SerialPort::read_available_characters(unsigned char * buffer, int count) {
+	requiresOpen();
+	int ret = vrpn_read_available_characters(_comm, buffer, count);
+	if (ret == -1) {
+		throw ReadFailure();
+	}
+
+	return ret;
+}
+
+std::string vrpn_SerialPort::read_available_characters(int count) {
+	std::string retString;
+	unsigned int numRead = 0;
+	unsigned int thisRead = 0;
+	static const unsigned int BUFSIZE = 256;
+	unsigned char buf[BUFSIZE];
+	int needed = BUFSIZE - 1;
+	do {
+		if (count > -1) {
+			needed = std::min(count - numRead, BUFSIZE - 1);
+		}
+		thisRead = read_available_characters(buf, needed);
+		if (thisRead != 0) {
+			retString.append(&(buf[0]), &(buf[0]) + thisRead);
+			numRead += thisRead;
+		}
+	} while (thisRead != 0 && (numRead < count || count == -1));
+	return retString;
+}
+
+int vrpn_SerialPort::read_available_characters(unsigned char * buffer, int count, struct timeval & timeout) {
+	requiresOpen();
+	int ret = vrpn_read_available_characters(_comm, buffer, count, &timeout);
+	if (ret == -1) {
+		throw ReadFailure();
+	}
+	return ret;
+}
+
+std::string vrpn_SerialPort::read_available_characters(int count, struct timeval & timeout) {
+	if (count + 1 < count) {
+		/// overflow safety
+		throw ReadFailure();
+	}
+	DynamicBufferType buf(count + 1);
+	int ret = read_available_characters(&(buf[0]), count, timeout);
+	return std::string(&(buf[0]), &(buf[0]) + ret);
+}
+
+void vrpn_SerialPort::flush_input_buffer() {
+	requiresOpen();
+	int ret = vrpn_flush_input_buffer(_comm);
+	if (ret == -1) {
+		throw FlushFailure();
+	}
+}
+
+void vrpn_SerialPort::flush_output_buffer() {
+	requiresOpen();
+	int ret = vrpn_flush_output_buffer(_comm);
+	if (ret == -1) {
+		throw FlushFailure();
+	}
+}
+
+void vrpn_SerialPort::drain_output_buffer() {
+	requiresOpen();
+	int ret = vrpn_drain_output_buffer(_comm);
+	if (ret == -1) {
+		throw DrainFailure();
+	}
+}
+
+void vrpn_SerialPort::set_rts() {
+	requiresOpen();
+	int ret = vrpn_set_rts(_comm);
+	if (ret == -1) {
+		throw RTSFailure();
+	}
+}
+
+void vrpn_SerialPort::clear_rts() {
+	requiresOpen();
+	int ret = vrpn_clear_rts(_comm);
+	if (ret == -1) {
+		throw RTSFailure();
+	}
+}
---
+++ vrpn_SerialPort.h
@@ -0,0 +1,196 @@
+/** @file
+	@brief Header
+
+	@date 2012
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef INCLUDED_vrpn_SerialPort_h_GUID_5d4c63a9_e038_4893_bfd7_116c43950dfa
+#define INCLUDED_vrpn_SerialPort_h_GUID_5d4c63a9_e038_4893_bfd7_116c43950dfa
+
+// Internal Includes
+#include "vrpn_Shared.h"
+#include "vrpn_Serial.h"
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+#include <stdexcept>
+#include <string>
+
+/// @brief A simple class wrapping the functionality of vrpn_Serial.h with
+/// RAII, object-orientation, and optional STL types
+class VRPN_API vrpn_SerialPort {
+	public:
+		typedef int file_handle_type;
+		/// @brief Construct and open port
+		/// @sa vrpn_open_commport
+		/// @throws OpenFailure
+		vrpn_SerialPort(const char * portname, long baud, int charsize = 8, vrpn_SER_PARITY parity = vrpn_SER_PARITY_NONE);
+
+		/// @brief Construct without opening
+		vrpn_SerialPort();
+
+		/// @brief Destructor - closes port if open.
+		~vrpn_SerialPort();
+
+		/// @name Open/Close Methods
+		/// @{
+		/// @brief Open serial port
+		/// @sa vrpn_open_commport
+		/// @throws OpenFailure, AlreadyOpen
+		void open(const char * portname, long baud, int charsize = 8, vrpn_SER_PARITY parity = vrpn_SER_PARITY_NONE);
+		bool is_open() const;
+
+		/// @brief Close the serial port.
+		/// @throws NotOpen, CloseFailure
+		void close();
+		/// @}
+
+		/// @name Write
+		/// @returns number of bytes written
+		/// @throws WriteFailure, NotOpen
+		/// @{
+		int write(std::string const& buffer);
+		int write(const unsigned char * buffer, int bytes);
+		/// @}
+
+		/// @name Read
+		/// @throws ReadFailure, NotOpen
+		/// @{
+		/// @brief Read available characters from input buffer, up to indicated count.
+		int read_available_characters(unsigned char * buffer, int count);
+
+		/// @brief Read available characters from input buffer, up to indicated count (or -1 for no limit)
+		std::string read_available_characters(int count = -1);
+
+		/// @brief Read available characters from input buffer, and wait up to the
+		/// indicated timeout for those remaining, up to indicated count.
+		int read_available_characters(unsigned char * buffer, int count, struct timeval & timeout);
+
+		/// @brief Read available characters from input buffer, and wait up to the
+		/// indicated timeout for those remaining, up to indicated count.
+		std::string read_available_characters(int count, struct timeval & timeout);
+		/// @}
+
+		/// @name Buffer manipulation
+		/// @{
+
+		/// @brief Throw out any characters within the input buffer.
+		/// @throws FlushFailure, NotOpen
+		void flush_input_buffer();
+
+		/// @brief Throw out any characters (do not send) within the output buffer.
+		/// @throws FlushFailure, NotOpen
+		void flush_output_buffer();
+
+		/// @brief Wait until all of the characters in the output buffer are sent, then return.
+		void drain_output_buffer();
+		/// @}
+
+		/// @name RTS
+		/// @brief Set and clear functions for the RTS ("ready to send") hardware flow- control bit.
+		///
+		/// These are used on a port that is already open. Some devices (like the Ascension Flock of Birds) use this to reset the device.
+		/// @throws RTSFailure, NotOpen
+		/// @{
+		void set_rts();
+		void clear_rts();
+		void assign_rts(bool set);
+		/// @}
+
+		/// @name Serial Port Exceptions
+		/// @{
+		struct AlreadyOpen;
+		struct NotOpen;
+		struct OpenFailure;
+		struct CloseFailure;
+		struct RTSFailure;
+		struct ReadFailure;
+		struct WriteFailure;
+		struct FlushFailure;
+		struct DrainFailure;
+		/// @}
+
+	private:
+		void requiresOpen() const;
+		/// @name Non-copyable
+		/// @{
+		vrpn_SerialPort(vrpn_SerialPort const &);
+		vrpn_SerialPort const & operator=(vrpn_SerialPort const &);
+		/// @}
+		file_handle_type _comm;
+		bool _rts_status;
+
+};
+
+struct vrpn_SerialPort::AlreadyOpen : std::logic_error {
+	AlreadyOpen() : std::logic_error("Tried to open a serial port that was already open.") {}
+};
+
+struct vrpn_SerialPort::NotOpen : std::logic_error {
+	NotOpen() : std::logic_error("Tried to use a serial port that was not yet open.") {}
+};
+
+struct vrpn_SerialPort::OpenFailure : std::runtime_error {
+	OpenFailure() : std::runtime_error("Received an error when trying to open serial port.") {}
+};
+
+struct vrpn_SerialPort::CloseFailure : std::runtime_error {
+	CloseFailure() : std::runtime_error("Received an error when trying to close serial port.") {}
+};
+
+struct vrpn_SerialPort::RTSFailure : std::runtime_error {
+	RTSFailure() : std::runtime_error("Failed to modify serial port RTS status.") {}
+};
+
+struct vrpn_SerialPort::ReadFailure : std::runtime_error {
+	ReadFailure() : std::runtime_error("Failure on serial port read.") {}
+};
+
+struct vrpn_SerialPort::WriteFailure : std::runtime_error {
+	WriteFailure() : std::runtime_error("Failure on serial port write.") {}
+};
+
+struct vrpn_SerialPort::FlushFailure : std::runtime_error {
+	FlushFailure() : std::runtime_error("Failure on serial port flush.") {}
+};
+
+struct vrpn_SerialPort::DrainFailure : std::runtime_error {
+	DrainFailure() : std::runtime_error("Failure on serial port drain.") {}
+};
+
+
+
+inline bool vrpn_SerialPort::is_open() const {
+	return _comm != -1;
+}
+
+inline void vrpn_SerialPort::assign_rts(bool set) {
+	if (set) {
+		set_rts();
+	} else {
+		clear_rts();
+	}
+}
+
+inline void vrpn_SerialPort::requiresOpen() const {
+	if (!is_open()) {
+		throw NotOpen();
+	}
+}
+
+#endif // INCLUDED_vrpn_SerialPort_h_GUID_5d4c63a9_e038_4893_bfd7_116c43950dfa
