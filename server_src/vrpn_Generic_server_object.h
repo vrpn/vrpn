61fac3cbe544fbe1fc231b94bcf08f72ffa5f37a
Russell M. Taylor II <taylorr@cs.unc.edu>
2012-07-16 14:53:26 -0400

    Adding Polhemus PDI configurations to vrpn.cfg and making it so the code compiles with VPRN_USE_PDI not defined.

--- server_src/vrpn_Generic_server_object.h
+++ server_src/vrpn_Generic_server_object.h
@@ -102,9 +102,7 @@
 //#include "vrpn_Cirque_Extended.h"
 
 // Polhemus additions
-#ifdef VRPN_USE_PDI
 #include "vrpn_Tracker_PDI.h"
-#endif
 
 #ifdef VRPN_USE_JSONNET
 #include "vrpn_Tracker_JsonNet.h"
@@ -358,11 +356,9 @@ class vrpn_Generic_Server_Object
     int setup_inertiamouse (char * & pch, char * line, FILE * config_file);
 
     // Polhemus additions
-#ifdef  VRPN_USE_PDI
     int setup_Tracker_G4(char* &pch, char* line, FILE* config_file); 
     int setup_Tracker_LibertyPDI(char* &pch, char* line, FILE* config_file); 
     int setup_Tracker_FastrakPDI(char* &pch, char* line, FILE* config_file); 
-#endif
 
 #ifdef VRPN_USE_JSONNET
     int setup_Tracker_JsonNet (char* &pch, char* line, FILE* config_file);
