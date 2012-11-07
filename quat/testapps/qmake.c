00a6060f60d9e85478a7e53bb75b2f831feb8124
Ryan Pavlik <rpavlik@iastate.edu>
2012-11-07 14:17:59 -0600

    #pragma warning requires _MSC_VER, not just _WIN32

--- quat/testapps/qmake.c
+++ quat/testapps/qmake.c
@@ -13,7 +13,9 @@
  *
  *****************************************************************************/
 
+#ifdef _MSC_VER
 #pragma warning( disable : 4996 ) // Don't complain about scanf
+#endif
 
 #include <stdio.h>
 #include "quat.h"
