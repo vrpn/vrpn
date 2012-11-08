4d92124fa92f7ca34cd8f371d7dcd861e06c197c
Russell M. Taylor II <taylorr@cs.unc.edu>
2012-11-08 08:30:01 -0500

    Making both examples do all three devices.

--- python/essai_2_7.py
+++ python/essai_2_7.py
@@ -7,14 +7,17 @@ def callback(userdata, data):
     print(toto)
 
 tracker=vrpn.receiver.Tracker("Tracker0@localhost")
-tracker.register_change_handler("workspace", callback, "workspace")
 tracker.register_change_handler("position", callback, "position")
 
 analog=vrpn.receiver.Analog("Input0@localhost")
 analog.register_change_handler("dev input", callback)
 
+button=vrpn.receiver.Button("Touchpad@localhost")
+button.register_change_handler("dev input", callback)
+
 while 1:
     tracker.mainloop()
     analog.mainloop()
+    button.mainloop()
 
 #del(essai)
--- python/essai_3_2.py
+++ python/essai_3_2.py
@@ -5,12 +5,17 @@ import vrpn
 def callback(userdata, data):
     print(userdata, " => ", data);
 
+tracker=vrpn.receiver.Tracker("Tracker0@localhost")
+tracker.register_change_handler("position", callback, "position")
+
 analog=vrpn.receiver.Analog("Touchpad@localhost")
 analog.register_change_handler("dev input", callback)
+
 button=vrpn.receiver.Button("Touchpad@localhost")
 button.register_change_handler("dev input", callback)
 
 while 1:
+    tracker.mainloop()
     analog.mainloop()
     button.mainloop()
 
