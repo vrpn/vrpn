6a39915a97476f4941f1bb416367d60b90b3bb44
Ryan Pavlik <rpavlik@iastate.edu>
2013-11-21 15:15:17 -0600

    Tweak config

--- util/formatFile.sh
+++ util/formatFile.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 clang-format -i \
-  -style="{BasedOnStyle: llvm, BreakConstructorInitializersBeforeComma: true, NamespaceIndentation: All, PointerBindsToType: true}" \
+  -style="{BasedOnStyle: llvm, AllowShortIfStatementsOnASingleLine: true, BreakBeforeBraces: Stroustrup, BreakConstructorInitializersBeforeComma: true, NamespaceIndentation: All, DerivePointerBinding: true}" \
   "$@"
