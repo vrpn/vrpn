bde2992272e6de4b5d0d1195f852dca00098d7d5
Kevin M. Godby <kevin@godby.org>
2014-12-20 15:00:01 -0600

    More work on Oculus Rift driver.

--- vrpn_Tracker_OculusRift.h
+++ vrpn_Tracker_OculusRift.h
@@ -20,7 +20,7 @@
 #include <OVR.h>
 
 // Standard includes
-// - none
+#include <string>
 
 #ifdef VRPN_USE_OVR
 
@@ -32,30 +32,51 @@
  */
 class VRPN_API vrpn_Tracker_OculusRift : public vrpn_Analog, public vrpn_Tracker
 {
-    public:
-        vrpn_Tracker_OculusRift(const char* name, vrpn_Connection* conn, int hmd_index = 0);
-        virtual ~vrpn_Tracker_OculusRift();
+public:
+    vrpn_Tracker_OculusRift(const char* name, vrpn_Connection* conn, int hmd_index = 0, const char* hmd_type = "Debug");
+    virtual ~vrpn_Tracker_OculusRift();
 
-        virtual void mainloop();
+    virtual void mainloop();
 
-    private:
-        enum {
-            ANALOG_CHANNELS = 1, // temperature
-            POSE_CHANNELS = 3    // head, camera, leveled camera
-        };
+private:
+    enum {
+        ANALOG_CHANNELS = 10, // accelerometer[3], gyro[3], magnetometer[3], temperature
+        POSE_CHANNELS = 3     // head, camera, leveled camera
+    };
 
-        void _get_tracking_state();
+    void _get_tracking_state();
 
-        ovrHmd _hmd;                        //< HMD device handle
+    /**
+     * Returns the readable name of the HMD type.
+     *
+     * @param hmd_type HMD type from ovrHmdType enum.
+     *
+     * @returns String of HMD type name.
+     */
+    std::string _get_hmd_type_name(ovrHmdType hmd_type) const;
 
-        q_xyz_quat_type _headPose;          //< head pose
-        q_xyz_quat_type _cameraPose;        //< camera pose
-        q_xyz_quat_type _leveledCameraPose; //< leveled camera pose
+    /**
+     * Returns the enum value of the HMD type.
+     *
+     * @param hmd_type The type of HMD. Acceptable values are: DK1, DK2, and
+     * Debug.
+     *
+     * @return The enum valuie of the hmd_type.
+     */
+    ovrHmdType _get_hmd_type(std::string hmd_type) const;
 
-        q_vec_type _accelerometer;          //< acceleration reading in m/s^2
-        q_vec_type _gyroscope;              //< rotation rate in rad/s
-        q_vec_type _magnetometer;           //< magnetic field in Gauss
-        float _temperature;                 //< temperature in degrees Celsius
+    ovrHmd _hmd;                        //< HMD device handle
+    ovrHmdType _hmdType;                //< requested HMD type
+
+
+    q_xyz_quat_type _headPose;          //< head pose
+    q_xyz_quat_type _cameraPose;        //< camera pose
+    q_xyz_quat_type _leveledCameraPose; //< leveled camera pose
+
+    q_vec_type _accelerometer;          //< acceleration reading in m/s^2
+    q_vec_type _gyroscope;              //< rotation rate in rad/s
+    q_vec_type _magnetometer;           //< magnetic field in Gauss
+    float _temperature;                 //< temperature in degrees Celsius
 };
 
 #else
