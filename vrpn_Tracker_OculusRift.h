e28f61780f68940874950d006beab41d99260f18
Kevin M. Godby <kevin@godby.org>
2014-12-20 23:41:33 -0600

    Set proper timestamps in Oculus Rift driver.

--- vrpn_Tracker_OculusRift.h
+++ vrpn_Tracker_OculusRift.h
@@ -38,8 +38,7 @@ typedef const ovrHmdDesc* ovrHmd;
  * gyroscope, acceleromter, and magnetometer. Starting with DK2, there is also
  * an external camera to track the HMD position.
  */
-class VRPN_API vrpn_Tracker_OculusRift : public vrpn_Analog, public vrpn_Tracker
-{
+class VRPN_API vrpn_Tracker_OculusRift : public vrpn_Analog, public vrpn_Tracker {
 public:
     vrpn_Tracker_OculusRift(const char* name, vrpn_Connection* conn, int hmd_index = 0, const char* hmd_type = "Debug");
     virtual ~vrpn_Tracker_OculusRift();
@@ -48,22 +47,14 @@ public:
 
 private:
     enum {
-        ANALOG_CHANNELS = 10, // accelerometer[3], gyro[3], magnetometer[3], temperature
-        POSE_CHANNELS = 3     // head, camera, leveled camera
+        ANALOG_CHANNELS = 10,  // accelerometer[3], gyro[3], magnetometer[3], temperature
+        POSE_CHANNELS = 3      // head, camera, leveled camera
     };
 
     void _get_tracking_state();
 
-    ovrHmd _hmd;              //< HMD device handle
-
-    q_xyz_quat_type _headPose;          //< head pose
-    q_xyz_quat_type _cameraPose;        //< camera pose
-    q_xyz_quat_type _leveledCameraPose; //< leveled camera pose
-
-    q_vec_type _accelerometer;          //< acceleration reading in m/s^2
-    q_vec_type _gyroscope;              //< rotation rate in rad/s
-    q_vec_type _magnetometer;           //< magnetic field in Gauss
-    float _temperature;                 //< temperature in degrees Celsius
+    ovrHmd _hmd;               //< HMD device handle
+    struct timeval _startTime; //< time at which OVR API was initialized
 };
 
 #else
