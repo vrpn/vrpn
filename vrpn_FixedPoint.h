a64f1f133705324701e05b88b5342cb65aef3193
Ryan Pavlik <ryan@sensics.com>
2014-12-30 11:38:37 -0600

    Namespace and format the HDK and fixed point code.

--- vrpn_FixedPoint.h
+++ vrpn_FixedPoint.h
@@ -20,123 +20,148 @@
 // Standard includes
 // - none
 
-namespace {
+namespace vrpn {
 
-/**
- * Set as the type member of vrpn_IntegerOfSize if too many bits are required.
- * We only support up to 32 bits currently.
- */
-struct vrpn_IntegerOverflow;
+    namespace detail {
+        /**
+         * Set as the type member of vrpn_IntegerOfSize if too many bits are
+         * required.
+         * We only support up to 32 bits currently.
+         */
+        struct IntegerOverflow;
 
-/**
- * \name Integer types.
- *
- * In the general case, we'll start adding bits to the required size until we
- * land on one of the specialized cases below, then return that specialized
- * case. The end result is that we return an integer type that can contain a
- * value require NUM_BITS bits.
- */
-//@{
-template <int NUM_BITS>
-struct vrpn_IntegerOfSize {
-    // An integer requiring n bits can be represented by an integer of size n+1 bits.
-    typedef typename vrpn_IntegerOfSize<NUM_BITS + 1>::type type;
-};
+        /**
+         * \name Integer types.
+         *
+         * In the general case, we'll start adding bits to the required size
+         *until we
+         * land on one of the specialized cases below, then return that
+         *specialized
+         * case. The end result is that we return an integer type that can
+         *contain a
+         * value require NUM_BITS bits.
+         */
+        ///@{
+        template <int NUM_BITS> struct IntegerOfSize {
+            // An integer requiring n bits can be represented by an integer of
+            // size n+1 bits.
+            typedef typename IntegerOfSize<NUM_BITS + 1>::type type;
+        };
 
-template <>
-struct vrpn_IntegerOfSize<8> {
-    typedef vrpn_int8 type;
-};
+        template <> struct IntegerOfSize<8> {
+            typedef vrpn_int8 type;
+        };
 
-template <>
-struct vrpn_IntegerOfSize<16> {
-    typedef vrpn_int16 type;
-};
+        template <> struct IntegerOfSize<16> {
+            typedef vrpn_int16 type;
+        };
 
-template <>
-struct vrpn_IntegerOfSize<32> {
-    typedef vrpn_int32 type;
-};
+        template <> struct IntegerOfSize<32> {
+            typedef vrpn_int32 type;
+        };
 
-template <>
-struct vrpn_IntegerOfSize<64> {
-    typedef vrpn_IntegerOverflow type;
-};
+        template <> struct IntegerOfSize<64> {
+            typedef IntegerOfSize type;
+        };
 
-// TODO allow for larger types if we can establish VRPN versions of them (e.g.,
-// equivalents to int64_t).
+        /// @todo allow for larger types if we can establish VRPN versions of
+        /// them (e.g.,
+        /// equivalents to int64_t).
 
-//@}
-
-} // end anonymous namespace
-
-
-/**
- * A fixed-point value class. All values are signed, two's-complement.
- *
- * @tparam INTEGER_BITS The number of bits devoted to the integer part.
- * @tparam FRACTIONAL_BITS The number of bits devoted to the fractioal part.
- */
-template <int INTEGER_BITS, int FRACTIONAL_BITS>
-class vrpn_FixedPoint {
-public:
-    /**
-     * Find an integer type large enough to hold INTEGER_BITS.
-     */
-    typedef typename vrpn_IntegerOfSize<INTEGER_BITS>::type IntegerType;
-
-    typedef typename vrpn_IntegerOfSize<INTEGER_BITS + FRACTIONAL_BITS>::type RawType;
+        ///@}
+    } // namespace detail
 
     /**
-     * \name Constructors.
+     * A fixed-point value class. All values are signed, two's-complement.
      *
-     * The bits of an integral type passed to the constructor will be
-     * interpreted as as fixed-point value. A floating-point type passed to the
-     * constructor will be converted to a fixed-point value.
+     * @tparam INTEGER_BITS The number of bits devoted to the integer part.
+     * @tparam FRACTIONAL_BITS The number of bits devoted to the fractioal part.
      */
-    //@{
-    vrpn_FixedPoint() : value_(0) { }
-    vrpn_FixedPoint(vrpn_int8 x) : value_(x) { }
-    vrpn_FixedPoint(vrpn_int16 x) : value_(x) { }
-    vrpn_FixedPoint(vrpn_int32 x) : value_(x) { }
-    vrpn_FixedPoint(vrpn_uint8 x) : value_(x) { }
-    vrpn_FixedPoint(vrpn_uint16 x) : value_(x) { }
-    vrpn_FixedPoint(vrpn_uint32 x) : value_(x) { }
-    vrpn_FixedPoint(double x) : value_(x * (1 << FRACTIONAL_BITS)) { }
-    vrpn_FixedPoint(float x) : value_(x * (1 << FRACTIONAL_BITS)) { }
-    //@}
+    template <int INTEGER_BITS, int FRACTIONAL_BITS> class FixedPoint {
+    public:
+        /**
+         * Find an integer type large enough to hold INTEGER_BITS.
+         */
+        typedef typename detail::IntegerOfSize<INTEGER_BITS>::type IntegerType;
 
-    // TODO add operators. lots and lots of operators.
+        typedef typename detail::IntegerOfSize<INTEGER_BITS +
+                                               FRACTIONAL_BITS>::type RawType;
 
-    /**
-     * Returns a floating-point representation of this
-     * fixed-point value.
-     */
-    //@{
-    operator vrpn_float32() const
-    {
-        return static_cast<vrpn_float32>(value_) / (1 << FRACTIONAL_BITS);
-    }
+        /**
+         * \name Constructors.
+         *
+         * The bits of an integral type passed to the constructor will be
+         * interpreted as as fixed-point value. A floating-point type passed to
+         *the
+         * constructor will be converted to a fixed-point value.
+         */
+        //@{
+        FixedPoint()
+            : value_(0)
+        {
+        }
+        FixedPoint(vrpn_int8 x)
+            : value_(x)
+        {
+        }
+        FixedPoint(vrpn_int16 x)
+            : value_(x)
+        {
+        }
+        FixedPoint(vrpn_int32 x)
+            : value_(x)
+        {
+        }
+        FixedPoint(vrpn_uint8 x)
+            : value_(x)
+        {
+        }
+        FixedPoint(vrpn_uint16 x)
+            : value_(x)
+        {
+        }
+        FixedPoint(vrpn_uint32 x)
+            : value_(x)
+        {
+        }
+        FixedPoint(double x)
+            : value_(x * (1 << FRACTIONAL_BITS))
+        {
+        }
+        FixedPoint(float x)
+            : value_(x * (1 << FRACTIONAL_BITS))
+        {
+        }
+        //@}
 
-    operator vrpn_float64() const
-    {
-        return static_cast<vrpn_float64>(value_) / (1 << FRACTIONAL_BITS);
-    }
-    //@}
+        // TODO add operators. lots and lots of operators.
 
-    // FIXME remove these functions after debugging
-    /** \name Debugging functions. */
-    //@{
-    RawType value() const
-    {
-        return value_;
-    }
-    //@}
+        /**
+         * Returns a floating-point representation of this
+         * fixed-point value.
+         */
+        //@{
+        operator vrpn_float32() const
+        {
+            return static_cast<vrpn_float32>(value_) / (1 << FRACTIONAL_BITS);
+        }
 
-private:
-    RawType value_;
+        operator vrpn_float64() const
+        {
+            return static_cast<vrpn_float64>(value_) / (1 << FRACTIONAL_BITS);
+        }
+        //@}
 
-};
+        // FIXME remove these functions after debugging
+        /** \name Debugging functions. */
+        //@{
+        RawType value() const { return value_; }
+        //@}
+
+    private:
+        RawType value_;
+    };
+
+} // namespace vrpn
 
 #endif // VRPN_FIXED_POINT_H_
-
--- vrpn_Tracker_OSVRHackerDevKit.C
+++ vrpn_Tracker_OSVRHackerDevKit.C
@@ -1,23 +1,23 @@
 /** @file	vrpn_Tracker_OSVRHackerDevKit.C
-	@brief	Implemendation of the OSVR Hacker Dev Kit HMD tracker.
+    @brief	Implemendation of the OSVR Hacker Dev Kit HMD tracker.
 
-	@date	2014
+    @date	2014
 
-	@author
-	Kevin M. Godby
-	<kevin@godby.org>
+    @author
+    Kevin M. Godby
+    <kevin@godby.org>
 */
 
 #include "vrpn_Tracker_OSVRHackerDevKit.h"
 
-#include "vrpn_BaseClass.h"             // for ::vrpn_TEXT_NORMAL, etc
-#include "vrpn_FixedPoint.h"            // for vrpn_fixed_to_float
-#include <quat.h>                       // for Q_W, Q_X, etc.
+#include "vrpn_BaseClass.h"  // for ::vrpn_TEXT_NORMAL, etc
+#include "vrpn_FixedPoint.h" // for vrpn_fixed_to_float
+#include <quat.h>            // for Q_W, Q_X, etc.
 
-#include <cstring>                      // for memset
-#include <iostream>                     // for operator<<, ostringstream, etc
+#include <cstring>  // for memset
+#include <iostream> // for operator<<, ostringstream, etc
 #include <sstream>
-#include <stdexcept>                    // for logic_error
+#include <stdexcept> // for logic_error
 
 VRPN_SUPPRESS_EMPTY_OBJECT_WARNING()
 
@@ -27,31 +27,35 @@ VRPN_SUPPRESS_EMPTY_OBJECT_WARNING()
 static const vrpn_uint16 vrpn_OSVR_VENDOR = 0x1532;
 static const vrpn_uint16 vrpn_OSVR_HACKER_DEV_KIT_HMD = 0x0b00;
 
-vrpn_Tracker_OSVRHackerDevKit::vrpn_Tracker_OSVRHackerDevKit(const char *name, vrpn_Connection *c) :
-    vrpn_Tracker(name, c),
-    vrpn_HidInterface(new vrpn_HidProductAcceptor(vrpn_OSVR_VENDOR, vrpn_OSVR_HACKER_DEV_KIT_HMD)),
-    _wasConnected(false)
+vrpn_Tracker_OSVRHackerDevKit::vrpn_Tracker_OSVRHackerDevKit(const char *name,
+                                                             vrpn_Connection *c)
+    : vrpn_Tracker(name, c)
+    , vrpn_HidInterface(new vrpn_HidProductAcceptor(
+          vrpn_OSVR_VENDOR, vrpn_OSVR_HACKER_DEV_KIT_HMD))
+    , _wasConnected(false)
 {
-        vrpn_Tracker::num_sensors = 1; // only orientation
+    vrpn_Tracker::num_sensors = 1; // only orientation
 
-        // Initialize the state
-        std::memset(d_quat, 0, sizeof(d_quat));
-        d_quat[Q_W] = 1.0;
+    // Initialize the state
+    std::memset(d_quat, 0, sizeof(d_quat));
+    d_quat[Q_W] = 1.0;
 
-        // Set the timestamp
-        vrpn_gettimeofday(&_timestamp, NULL);
-    }
+    // Set the timestamp
+    vrpn_gettimeofday(&_timestamp, NULL);
+}
 
 vrpn_Tracker_OSVRHackerDevKit::~vrpn_Tracker_OSVRHackerDevKit()
 {
     delete _acceptor;
 }
 
-void vrpn_Tracker_OSVRHackerDevKit::on_data_received(std::size_t bytes, vrpn_uint8 *buffer)
+void vrpn_Tracker_OSVRHackerDevKit::on_data_received(std::size_t bytes,
+                                                     vrpn_uint8 *buffer)
 {
     if (bytes != 32) {
         std::ostringstream ss;
-        ss << "Received a report " << bytes << " in length, but expected it to be 32 bytes.";
+        ss << "Received a report " << bytes
+           << " in length, but expected it to be 32 bytes.";
         struct timeval ts;
         vrpn_gettimeofday(&ts, NULL);
         send_text_message(ss.str().c_str(), ts, vrpn_TEXT_WARNING);
@@ -62,16 +66,24 @@ void vrpn_Tracker_OSVRHackerDevKit::on_data_received(std::size_t bytes, vrpn_uin
     vrpn_uint8 msg_seq = vrpn_unbuffer_from_little_endian<vrpn_uint8>(buffer);
 
     // Signed, 16-bit, fixed-point numbers in Q1.14 format.
-    d_quat[Q_X] = static_cast<double>(vrpn_FixedPoint<1, 14>(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
-    d_quat[Q_Y] = static_cast<double>(vrpn_FixedPoint<1, 14>(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
-    d_quat[Q_Z] = static_cast<double>(vrpn_FixedPoint<1, 14>(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
-    d_quat[Q_W] = static_cast<double>(vrpn_FixedPoint<1, 14>(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
+    typedef vrpn::FixedPoint<1, 14> FixedPointValue;
+    d_quat[Q_X] = static_cast<double>(
+        FixedPointValue(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
+    d_quat[Q_Y] = static_cast<double>(
+        FixedPointValue(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
+    d_quat[Q_Z] = static_cast<double>(
+        FixedPointValue(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
+    d_quat[Q_W] = static_cast<double>(
+        FixedPointValue(vrpn_unbuffer_from_little_endian<vrpn_int16>(buffer)));
 
     vrpn_Tracker::timestamp = _timestamp;
     char msgbuf[512];
     int len = vrpn_Tracker::encode_to(msgbuf);
-    if (d_connection->pack_message(len, _timestamp, position_m_id, d_sender_id, msgbuf, vrpn_CONNECTION_LOW_LATENCY)) {
-        fprintf(stderr, "vrpn_Tracker_OSVRHackerDevKit: cannot write message: tossing\n");
+    if (d_connection->pack_message(len, _timestamp, position_m_id, d_sender_id,
+                                   msgbuf, vrpn_CONNECTION_LOW_LATENCY)) {
+        fprintf(
+            stderr,
+            "vrpn_Tracker_OSVRHackerDevKit: cannot write message: tossing\n");
     }
 }
 
@@ -82,7 +94,8 @@ void vrpn_Tracker_OSVRHackerDevKit::mainloop()
     update();
 
     if (connected() && !_wasConnected) {
-        send_text_message("Successfully connected to OSVR Hacker Dev Kit HMD.", _timestamp, vrpn_TEXT_NORMAL);
+        send_text_message("Successfully connected to OSVR Hacker Dev Kit HMD.",
+                          _timestamp, vrpn_TEXT_NORMAL);
     }
     _wasConnected = connected();
 
@@ -90,4 +103,3 @@ void vrpn_Tracker_OSVRHackerDevKit::mainloop()
 }
 
 #endif
-
