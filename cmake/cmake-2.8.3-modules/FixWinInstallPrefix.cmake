5b67ce1204fec82e75bbd327f151cba535c9dc18
Ryan Pavlik <ryan@sensics.com>
2015-02-03 11:34:22 -0600

    Update CMake modules, syncing changes and fixing warnings with CMake >=3.1

--- cmake/cmake-2.8.3-modules/FixWinInstallPrefix.cmake
+++ cmake/cmake-2.8.3-modules/FixWinInstallPrefix.cmake
@@ -1,6 +1,6 @@
 # - Automatically fix CMAKE_INSTALL_PREFIX to be bit-appropriate on Win
 #
-# This is a workaround for CMake bug #9992 in at least <=2.8.0 - see
+# This is a workaround for CMake bug #9992 in <2.8.3 - see
 # http://public.kitware.com/Bug/view.php?id=9992
 #
 # It runs automatically when included on a Windows build (passes if(WIN32)) -
@@ -27,7 +27,8 @@ if(WIN32)
 	file(TO_CMAKE_PATH "$ENV{ProgramFiles}" _PROG_FILES)
 
 	# 32-bit dir: only set on win64
-	file(TO_CMAKE_PATH "$ENV{ProgramFiles(x86)}" _PROG_FILES_X86)
+    set(_PF86 "ProgramFiles(x86)")
+	file(TO_CMAKE_PATH "$ENV{${_PF86}}" _PROG_FILES_X86)
 
 	# 64-bit dir: only set on win64
 	file(TO_CMAKE_PATH "$ENV{ProgramW6432}" _PROG_FILES_W6432)
