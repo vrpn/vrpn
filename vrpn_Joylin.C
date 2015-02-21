4c10cee880996b8f018bade6f65eab8c3239e921
Russ Taylor <russ@reliasolve.com>
2015-02-21 12:04:00 -0500

    Fix to let code compile on other than Linux.

--- vrpn_Joylin.C
+++ vrpn_Joylin.C
@@ -155,7 +155,7 @@ void vrpn_Joylin::mainloop(void)
 
 vrpn_Joylin::vrpn_Joylin(const char * name, 
 			 vrpn_Connection * c,
-			 char *):
+			 const char *):
   vrpn_Analog(name, c), vrpn_Button_Filter(name, c)
 { 
 	  fprintf(stderr,"vrpn_Joylin::vrpn_Joylin: Can't open Linux joystick on non-Linux machine\n");
