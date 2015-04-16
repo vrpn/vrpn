94c527440e797682fcd322dad1d01fa7c8eed6e0
Russ Taylor <russ@reliasolve.com>
2015-04-16 08:49:16 -0400

    Added a program to display tracker sensor orientations from Gustaf Hendeby and Dmitry Mastykin at Trivisio, tweaked to work with any tracker.

--- client_src/testimager_client.C
+++ client_src/testimager_client.C
@@ -34,7 +34,7 @@ int			g_Xdim, g_Ydim;	//< Dimensions in X and Y
 bool g_ready_for_region = false;	//< Everything set up to handle a region?
 unsigned char *g_image = NULL;		//< Pointer to the storage for the image
 bool g_already_posted = false;		//< Posted redisplay since the last display?
-bool g_autoscale = false;               //< Should we auto-scale the brightness and contrast?
+bool g_autoscale = false;           //< Should we auto-scale the brightness and contrast?
 
 //----------------------------------------------------------------------------
 // Imager callback handlers.
